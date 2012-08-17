4975bbf14141c28b7f4a8777aff6b9f50d838a81
zomeki <zomeki@zomeki.jp>
2012-08-17 23:30:22 +0900

    インストールスクリプトを再作成

---
+++ doc/install_scripts/install_zomeki_kana_read.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+DONE_FLAG="/tmp/zomeki_$0_done"
+
+echo '#### Install ZOMEKI (kana, read) ####'
+if [ -f $DONE_FLAG ]; then exit; fi
+echo '-- PRESS ANY KEY --'
+read KEY
+
+ubuntu() {
+  echo 'Ubuntu will be supported shortly.'
+}
+
+centos() {
+  echo "It's CentOS!"
+
+  cd /usr/local/src
+  rm -rf darts-0.32.tar.gz darts-0.32
+  wget http://chasen.org/~taku/software/darts/src/darts-0.32.tar.gz
+  tar zxf darts-0.32.tar.gz && cd darts-0.32 && ./configure && make && make install
+
+  cd /usr/local/src
+  rm -rf lame-3.99.1.tar.gz lame-3.99.1
+  wget http://jaist.dl.sourceforge.net/project/lame/lame/3.99/lame-3.99.1.tar.gz
+  tar zxf lame-3.99.1.tar.gz && cd lame-3.99.1 && ./configure && make && make install
+
+  cd /usr/local/src
+  rm -rf chasen-2.4.4.tar.gz chasen-2.4.4
+  wget http://jaist.dl.sourceforge.jp/chasen-legacy/32224/chasen-2.4.4.tar.gz
+  tar zxf chasen-2.4.4.tar.gz && cd chasen-2.4.4 && ./configure && make && make install
+
+  yum install -y nkf
+  cd /usr/local/src
+  rm -rf ipadic-2.7.0.tar.gz ipadic-2.7.0
+  wget http://iij.dl.sourceforge.jp/ipadic/24435/ipadic-2.7.0.tar.gz
+  tar zxf ipadic-2.7.0.tar.gz && cd ipadic-2.7.0 && ./configure
+cat <<'EOF' > to_utf8.sh
+#!/bin/sh
+for file in *.dic *.cha chasenrc
+do
+if [ -f $file ]; then
+  nkf --utf8 $file > tmpfile
+  mv tmpfile $file
+fi
+done
+exit
+EOF
+  chmod 755 to_utf8.sh
+  ./to_utf8.sh
+  ldconfig
+  `chasen-config --mkchadic`/makemat -i w
+  `chasen-config --mkchadic`/makeda -i w chadic *.dic
+  make install
+
+  yum install -y libxslt-devel
+  cd /var/share/zomeki/ext/morph/chaone
+  chmod 775 configure
+  ./configure && make && make install
+
+  cd /var/share/zomeki/ext/gtalk
+  chmod 775 configure
+  ./configure && make
+  chmod 755 /var/share/zomeki/ext/gtalk_filter.rb
+}
+
+others() {
+  echo 'This OS is not supported.'
+  exit
+}
+
+if [ -f /etc/lsb-release ]; then
+  if grep -qs Ubuntu /etc/lsb-release; then
+    ubuntu
+  else
+    others
+  fi
+elif [ -f /etc/centos-release ]; then
+  centos
+else
+  others
+fi
+
+touch $DONE_FLAG
---
+++ doc/install_scripts/prepare.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+if [ -z "$ZOMEKI_VERSION" ]; then ZOMEKI_VERSION='v1.0.2'; fi
+
+INSTALL_SCRIPTS_URL="https://raw.github.com/zomeki/zomeki/${ZOMEKI_VERSION}/doc/install_scripts"
+
+echo '#### Prepare to install ####'
+
+ubuntu() {
+  echo 'Ubuntu will be supported shortly.'
+}
+
+centos() {
+  echo "It's CentOS!"
+
+  rpm -ivh http://dl.fedoraproject.org/pub/epel/6/`uname -i`/epel-release-6-7.noarch.rpm
+  yum install -y wget
+
+  cd /usr/local/src
+
+  files=('install_ruby.sh' 'install_zomeki.sh' 'install_apache.rb' 'install_mysql.rb'
+         'configure_zomeki.rb' 'install_zomeki_kana_read.sh' 'start_servers.sh' 'install_cron.sh')
+
+  rm -f install_scripts.txt
+  for file in ${files[@]}; do
+    echo "$INSTALL_SCRIPTS_URL/$file" >> install_scripts.txt
+  done
+
+  wget -i install_scripts.txt
+
+  for file in ${files[@]}; do
+    chmod 755 $file
+  done
+
+  rm -f install_all.sh
+  for file in ${files[@]}; do
+    echo "./$file" >> install_all.sh
+  done
+cat <<'EOF' >> install_all.sh
+
+echo "
+-- インストールを完了しました。 --
+
+  公開画面: `ruby -ryaml -e "puts YAML.load_file('/var/share/zomeki/config/core.yml')['production']['uri']"`
+
+  管理画面: `ruby -ryaml -e "puts YAML.load_file('/var/share/zomeki/config/core.yml')['production']['uri']"`_system
+    ユーザID: zomeki
+    パスワード: zomeki
+
+１．MySQL の root ユーザはパスワードが rootpass に設定されています。適宜変更してください。
+    # mysqladmin -u root -prootpass password 'newpass'
+２．MySQL の zomeki ユーザはパスワードが zomekipass に設定されています。適宜変更してください。
+    mysql> SET PASSWORD FOR zomeki@localhost = PASSWORD('newpass');
+    また、変更時には /var/share/zomeki/config/database.yml も合わせて変更してください。
+    # vi /var/share/zomeki/config/database.yml
+３．OS の zomeki ユーザに cron が登録されています。運用時には有効化してください。
+    # crontab -u zomeki -e
+"
+EOF
+  chmod 755 install_all.sh
+
+echo '
+-- インストールを続けるには以下のコマンドを実行してください。 --
+
+# cd /usr/local/src && /usr/local/src/install_all.sh
+'
+}
+
+others() {
+  echo 'This OS is not supported.'
+  exit
+}
+
+if [ -f /etc/lsb-release ]; then
+  if grep -qs Ubuntu /etc/lsb-release; then
+    ubuntu
+  else
+    others
+  fi
+elif [ -f /etc/centos-release ]; then
+  centos
+else
+  others
+fi
