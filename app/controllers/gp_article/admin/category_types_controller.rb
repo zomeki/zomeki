cbd05970dad9177cacdd3d705f8c2ad6620eb4a8
zomeki <zomeki@zomeki.jp>
2012-11-06 02:47:37 +0900

    Fix rise exception if content not found

--- app/controllers/gp_article/admin/category_types_controller.rb
+++ app/controllers/gp_article/admin/category_types_controller.rb
@@ -3,7 +3,7 @@ class GpArticle::Admin::CategoryTypesController < Cms::Controller::Admin::Base
   include Sys::Controller::Scaffold::Base
 
   def pre_dispatch
-    return error_auth unless @content = GpArticle::Content::Doc.find(params[:content])
+    return error_auth unless @content = GpArticle::Content::Doc.find_by_id(params[:content])
     return error_auth unless Core.user.has_priv?(:read, :item => @content.concept)
     return redirect_to(request.env['PATH_INFO']) if params[:reset]
   end
