2071abdb3781d8c417548c9385816085fc4ae542
zomeki <zomeki@zomeki.jp>
2012-11-08 13:31:35 +0900

    Add category trees

--- app/models/gp_article/category.rb
+++ app/models/gp_article/category.rb
@@ -22,8 +22,24 @@ class GpArticle::Category < ActiveRecord::Base
 
   belongs_to :status, :foreign_key => :state, :class_name => 'Sys::Base::Status'
 
+  has_and_belongs_to_many :docs, :class_name => 'GpArticle::Doc', :join_table => 'gp_article_categories_gp_article_docs'
+
   validates :name, :presence => true, :uniqueness => {:scope => :content_id}
   validates :title, :presence => true
 
-  default_scope order(:level_no, :sort_no)
+  default_scope order(:category_type_id, :level_no, :sort_no)
+
+  def descendants(selected_ids=[])
+    tree = {key: id, title: title, tooltip: name}
+
+    if children.empty?
+      tree[:select] = selected_ids.include?(id)
+    else
+      tree[:expand] = true
+      tree[:children] = children.map {|c| c.descendants(selected_ids) }
+      tree[:select] = tree[:children].all? {|c| c[:select] }
+    end
+
+    return tree
+  end
 end
