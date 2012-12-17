9b475d44f716f940cb618781617caa5adb372a5d
zomeki <zomeki@zomeki.jp>
2012-12-18 06:58:04 +0900

    Rvise category settings

--- db/migrate/20121214080240_create_gp_category_categories.rb
+++ db/migrate/20121214080240_create_gp_category_categories.rb
@@ -2,6 +2,8 @@ class CreateGpCategoryCategories < ActiveRecord::Migration
   def change
     create_table :gp_category_categories do |t|
       t.integer    :unid
+
+      t.references :concept
       t.references :layout
 
       t.references :category_type
@@ -11,9 +13,11 @@ class CreateGpCategoryCategories < ActiveRecord::Migration
       t.string     :title
       t.integer    :level_no
       t.integer    :sort_no
+      t.string     :description
 
       t.timestamps
     end
+    add_index :gp_category_categories, :concept_id
     add_index :gp_category_categories, :layout_id
     add_index :gp_category_categories, :category_type_id
     add_index :gp_category_categories, :parent_id
