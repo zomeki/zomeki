da4876b9cd4132cbb1d90042258f0ec9722617a1
zomeki <zomeki@zomeki.jp>
2012-12-24 22:16:50 +0900

    Now attachment file available on public side

--- app/controllers/gp_article/public/node/docs_controller.rb
+++ app/controllers/gp_article/public/node/docs_controller.rb
@@ -3,6 +3,8 @@
 require 'will_paginate/array'
 
 class GpArticle::Public::Node::DocsController < Cms::Controller::Public::Base
+  skip_filter :render_public_layout, :only => [:file_content]
+
   def pre_dispatch
     @content = GpArticle::Content::Doc.find_by_id(Page.current_node.content.id)
     return http_error(404) unless @content
@@ -14,7 +16,16 @@ class GpArticle::Public::Node::DocsController < Cms::Controller::Public::Base
   end
 
   def show
-    @doc = @content.docs.find_by_id(params[:id])
+    @doc = @content.docs.find_by_name(params[:name])
     return http_error(404) unless @doc
+  end
+
+  def file_content
+    @doc = @content.docs.find_by_name(params[:name])
+    if (file = Sys::File.where(parent_unid: @doc.unid, name: "#{params[:basename]}.#{params[:extname]}").first)
+      send_file file.upload_path, :type => file.mime_type, :filename => file.name, :disposition => 'attachment'
+    else
+      http_error(404)
+    end
   end
 end
