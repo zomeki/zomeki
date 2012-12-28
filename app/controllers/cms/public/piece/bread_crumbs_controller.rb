be7e3187d35a44aaf2613a82b8960a53274c3611
zomeki <zomeki@zomeki.jp>
2012-12-28 17:35:57 +0900

    Add bread crumbs to category_type

--- app/controllers/cms/public/piece/bread_crumbs_controller.rb
+++ app/controllers/cms/public/piece/bread_crumbs_controller.rb
@@ -2,7 +2,7 @@
 class Cms::Public::Piece::BreadCrumbsController < Sys::Controller::Public::Base
   def index
     @item = Page.current_item
-    if defined?(@item.bread_crumbs)
+    if @item.respond_to?(:bread_crumbs)
       @bread_crumbs = @item.bread_crumbs(Page.current_node)
     else
       @bread_crumbs = Page.current_node.bread_crumbs
