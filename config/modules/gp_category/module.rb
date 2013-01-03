64e2f78dfe04e70afe4ee507e98202cf4a2adf4b
zomeki <zomeki@zomeki.jp>
2013-01-04 06:02:42 +0900

    Create category_types piece admin side

--- config/modules/gp_category/module.rb
+++ config/modules/gp_category/module.rb
@@ -5,4 +5,7 @@ Cms::Lib::Modules::ModuleSet.draw :gp_category, '汎用カテゴリ' do |mod|
 
   ## directories
   mod.directory :category_types, '汎用カテゴリタイプページ'
+
+  ## pieces
+  mod.piece :category_types, '汎用カテゴリ一覧'
 end
