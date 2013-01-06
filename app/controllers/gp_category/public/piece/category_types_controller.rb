9e07d05831047dd2f6be53bd5fd022bb60673798
zomeki <zomeki@zomeki.jp>
2013-01-06 21:46:30 +0900

    Remove not used variable

--- app/controllers/gp_category/public/piece/category_types_controller.rb
+++ app/controllers/gp_category/public/piece/category_types_controller.rb
@@ -2,8 +2,7 @@
 class GpCategory::Public::Piece::CategoryTypesController < Sys::Controller::Public::Base
   def pre_dispatch
     @content = GpCategory::Content::CategoryType.find_by_id(Page.current_piece.content_id)
-    @node = @content.try(:category_type_node)
-    return http_error(404) unless @node
+    return http_error(404) unless @content
   end
 
   def index
