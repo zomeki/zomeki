04973ad5e1358feec8255e88095179acf23a290c
zomeki <zomeki@zomeki.jp>
2013-01-10 06:41:32 +0900

    Add category_type/category options to piece

---
+++ app/models/gp_category/piece/category_type.rb
@@ -0,0 +1,36 @@
+# encoding: utf-8
+class GpCategory::Piece::CategoryType < Cms::Piece
+  LAYER_OPTIONS = [['下層のカテゴリすべて', 'descendants'], ['該当カテゴリのみ', 'self']]
+
+  default_scope where(model: 'GpCategory::CategoryType')
+
+  def content
+    GpCategory::Content::CategoryType.find(super.id)
+  end
+
+  def category_types
+    content.category_types
+  end
+
+  def category_types_for_option
+    category_types.map {|ct| [ct.title, ct.id] }
+  end
+
+  def category_type
+    category_types.find_by_id(setting_value(:category_type_id))
+  end
+
+  def categories
+    return [] unless category_type
+    category_type.categories.where(id: setting_value(:category_id))
+  end
+
+  def category
+    return nil if categories.empty? || !categories.respond_to?(:find_by_id)
+    categories.find_by_id(setting_value(:category_id))
+  end
+
+  def layer
+    setting_value(:layer) || LAYER_OPTIONS.first.last
+  end
+end
