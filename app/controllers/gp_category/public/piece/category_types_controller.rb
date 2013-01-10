57d876f1ba0a349b31f952bc5ded5139ea997426
zomeki <zomeki@zomeki.jp>
2013-01-10 12:12:38 +0900

    Move current_item to pre_dispatch

--- app/controllers/gp_category/public/piece/category_types_controller.rb
+++ app/controllers/gp_category/public/piece/category_types_controller.rb
@@ -3,9 +3,10 @@ class GpCategory::Public::Piece::CategoryTypesController < Sys::Controller::Publ
   def pre_dispatch
     @piece = GpCategory::Piece::CategoryType.find_by_id(Page.current_piece.id)
     return http_error(404) unless @piece
+
+    @item = Page.current_item
   end
 
   def index
-    @item = Page.current_item
   end
 end
