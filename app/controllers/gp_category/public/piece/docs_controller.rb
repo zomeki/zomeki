32092e55ab882c6a0dd470bf6412347736ec900b
zomeki <zomeki@zomeki.jp>
2013-01-10 17:45:22 +0900

    Just refactoring

--- app/controllers/gp_category/public/piece/docs_controller.rb
+++ app/controllers/gp_category/public/piece/docs_controller.rb
@@ -14,7 +14,7 @@ class GpCategory::Public::Piece::DocsController < Sys::Controller::Public::Base
       piece_category_ids = @piece.categories.map(&:id)
     end
 
-    piece_doc_ids = GpArticle::Doc.where(state: 'public').joins('INNER JOIN gp_article_docs_gp_category_categories AS gadgcc ON gp_article_docs.id = gadgcc.doc_id').where('gadgcc.category_id' => piece_category_ids).map(&:id)
+    piece_doc_ids = find_public_docs_by_category_ids(piece_category_ids).map(&:id)
 
     case @item
     when GpCategory::CategoryType
@@ -24,12 +24,18 @@ class GpCategory::Public::Piece::DocsController < Sys::Controller::Public::Base
     end
 
     if page_category_ids
-      page_doc_ids = GpArticle::Doc.where(state: 'public').joins('INNER JOIN gp_article_docs_gp_category_categories AS gadgcc ON gp_article_docs.id = gadgcc.doc_id').where('gadgcc.category_id' => page_category_ids).map(&:id)
+      page_doc_ids = find_public_docs_by_category_ids(page_category_ids).map(&:id)
       doc_ids = piece_doc_ids & page_doc_ids
     else
       doc_ids = piece_doc_ids
     end
 
     @docs = GpArticle::Doc.where(id: doc_ids).order('published_at DESC').limit(@piece.list_count)
+  end
+
+  private
+
+  def find_public_docs_by_category_ids(category_ids)
+    GpArticle::Doc.where(state: 'public').joins('INNER JOIN gp_article_docs_gp_category_categories AS gadgcc ON gp_article_docs.id = gadgcc.doc_id').where('gadgcc.category_id' => category_ids)
   end
 end
