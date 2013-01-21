72db19c4474d52b223a9f7b7aefdbf1eb323110e
zomeki <zomeki@zomeki.jp>
2013-01-21 09:07:26 +0900

    Add settings for gnav content

---
+++ app/models/gnav/content/setting.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+class Gnav::Content::Setting < Cms::ContentSetting
+  set_config :gp_category_content_category_type_id, :name => '汎用カテゴリタイプ',
+    :options => GpCategory::Content::CategoryType.all.map{|ct| [ct.name, ct.id] }
+
+  def upper_text
+  end
+
+  def lower_text
+  end
+end
