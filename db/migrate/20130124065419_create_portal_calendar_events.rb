a0c018ee3154a60559ba8aa11f1204b9647b74f3
Kouichi AZUMA <azuma@idsinc.co.jp>
2013-01-24 17:38:32 +0900

    ポータルイベントの更新

--- db/migrate/20130124065419_create_portal_calendar_events.rb
+++ db/migrate/20130124065419_create_portal_calendar_events.rb
@@ -9,15 +9,35 @@ class CreatePortalCalendarEvents < ActiveRecord::Migration
       t.string   :event_uri
       t.string   :title
       t.text     :body,         :limit => 2147483647
+			t.integer  :genre_id,     :default => 0
+			t.integer  :status_id,    :default => 0
+			
+			t.timestamps
+    end
+    add_index :portal_calendar_events, [:content_id, :published_at, :event_date], :name => :content_id
 
+		create_table :portal_calendar_genres do |t|
+      t.integer  :unid
+      t.integer  :content_id
+      t.string   :state,     :default => 'public'
+      t.string   :title,     :default => ''
+			
 			t.timestamps
     end
 
-    add_index :portal_calendar_events, [:content_id, :published_at, :event_date], :name => :content_id
-
+		create_table :portal_calendar_statuses do |t|
+      t.integer  :unid
+      t.integer  :content_id
+      t.string   :state,     :default => 'public'
+      t.string   :title,     :default => ''
+			
+			t.timestamps
+    end
   end
 
   def down
 		drop_table :portal_calendar_events
+		drop_table :portal_calendar_genres
+		drop_table :portal_calendar_statuses
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -811,11 +811,31 @@ ActiveRecord::Schema.define(:version => 20130124065419) do
     t.string   "event_uri"
     t.string   "title"
     t.text     "body",         :limit => 2147483647
+    t.integer  "genre_id",                           :default => 0
+    t.integer  "status_id",                          :default => 0
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "portal_calendar_events", ["content_id", "published_at", "event_date"], :name => "content_id"
+
+  create_table "portal_calendar_genres", :force => true do |t|
+    t.integer  "unid"
+    t.integer  "content_id"
+    t.string   "state",      :default => "public"
+    t.string   "title",      :default => ""
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "portal_calendar_statuses", :force => true do |t|
+    t.integer  "unid"
+    t.integer  "content_id"
+    t.string   "state",      :default => "public"
+    t.string   "title",      :default => ""
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "portal_categories", :force => true do |t|
     t.integer  "unid"
