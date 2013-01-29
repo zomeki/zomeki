b6d0b774d8a7ad12b9cc77c219c893bbe094a51e
zomeki <zomeki@zomeki.jp>
2013-01-30 00:26:50 +0900

    Update list_style samples

--- app/models/gp_article/content/setting.rb
+++ app/models/gp_article/content/setting.rb
@@ -7,7 +7,7 @@ class GpArticle::Content::Setting < Cms::ContentSetting
   set_config :recognition_type, :name => '承認/承認フロー',
     :options => [['管理者承認が必要', 'with_admin']]
   set_config :list_style, :name => "#{GpArticle::Doc.model_name.human}表示形式",
-    :comment => '日付：@date タイトル：@title 組織：@group'
+    :comment => '日付：@date タイトル：@title 組織：@group カテゴリ：@category'
   set_config :date_style, :name => "#{GpArticle::Doc.model_name.human}日付形式",
     :comment => '年：%Y 月：%m 日：%d 時：%H 分：%M 秒：%S'
 
