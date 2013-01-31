bbaeb09a9fb536ef33cbf9236344b8871d0cff6d
Kouichi AZUMA <azuma@idsinc.co.jp>
2013-01-31 14:49:53 +0900

    イベントカレンダーの本日明日ピース作成

--- app/controllers/portal_calendar/public/piece/event_links_controller.rb
+++ app/controllers/portal_calendar/public/piece/event_links_controller.rb
@@ -1,40 +1,29 @@
 # encoding: utf-8
 class PortalCalendar::Public::Piece::EventLinksController < Sys::Controller::Public::Base
-  def index
+	def index
+
     @content  = PortalCalendar::Content::Base.find(Page.current_piece.content_id)
     @node     = @content.event_node
     @node_uri = @node.public_uri if @node
-    return render(:text => '') unless @node
+		return render(:text => '') unless @node
     
-    @min_date = params[:calendar_event_min_date]
-    @max_date = params[:calendar_event_max_date]
-    @year     = params[:calendar_event_year]
-    @month    = params[:calendar_event_month]
-    @links = []
-    return render(:text => '') unless @min_date
-    
-    year = nil
-    date = @min_date
-    while date <= @max_date
-      month = date.month
-      if year != date.year
-        year  = date.year
-        ccls  = "year year#{year}"
-        ccls += " current" if @year == date.year && @month.nil?
-        @links << {
-          :name  => date.strftime("%Y年"),
-          :uri   => "#{@node_uri}#{year}/",
-          :class => ccls
-        }
-      end
-      ccls  = "month month#{month}"
-      ccls += " current" if @year == date.year && @month == month
-      @links << {
-        :name  => date.strftime("%-m月"),
-        :uri   => "#{@node_uri}#{year}/" + sprintf("%02d/", month),
-        :class  => ccls
-      }
-      date = date >> 1
-    end
-  end
+    @today    = Date.today
+		@events_list = get_events(@content.id, @today, 1)
+	end
+
+private
+
+	#指定日からndays間のイベントを取得する
+	def get_events(content_id, basedate, ndays)
+		sdate = basedate
+		edate = basedate + ndays
+
+		events = []
+		(sdate .. edate).each do |day|
+			# TODO: DBから一回で取ってスクリプトで日ごとに分解する方が早いかも。DBの負荷が高くないならこちらの方がすっきりしている。
+			events << PortalCalendar::Event.get_period_records_with_content_id(content_id, day, day)
+		end
+		return events
+	end
+
 end
