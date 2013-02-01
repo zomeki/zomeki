01ecd0431335f7488e321624a70c19f793effc3b
Kouichi AZUMA <azuma@idsinc.co.jp>
2013-02-01 14:31:00 +0900

    イベントカレンダー ・最初にページを開いたときに全データ抽出の条件を設定しておく ・API対応（XMLフォーマット出力）

--- app/controllers/portal_calendar/public/node/events_controller.rb
+++ app/controllers/portal_calendar/public/node/events_controller.rb
@@ -15,7 +15,9 @@ class PortalCalendar::Public::Node::EventsController < Cms::Controller::Public::
 		@max_row = 5
 		@max_column = 7 - 1
 		@base_nbr = 0
-		
+
+		#GETで開いたときはページを初期状態で開く
+		@init_page = /GET/ =~ request.headers['REQUEST_METHOD']
   end
 
 	#要素を整数化した配列を返す
@@ -34,14 +36,19 @@ class PortalCalendar::Public::Node::EventsController < Cms::Controller::Public::
     return http_error(404) unless validate_date
     return http_error(404) if Date.new(@year, @month, 1) < @min_date
     return http_error(404) if Date.new(@year, @month, 1) > @max_date
-
-		#前後の月の抽出条件を引き継ぐ egnr=event genres, estt=event statuses
-		_genres = params[:egnr].nil? ? [] : params[:egnr].split(",")
-		_statuses = params[:estt].nil? ? [] : params[:estt].split(",")
 		
-		#フォームでsubmitされたときはフォームの抽出条件で処理する
-		@genre_keys = params[:genre].nil? ? conv_to_i(_genres) : conv_to_i(params[:genre].keys)
-		@status_keys = params[:status].nil? ? conv_to_i(_statuses) : conv_to_i(params[:status].keys)
+		if @init_page
+			@genre_keys = @genres.map{|item| item.id}
+			@status_keys = @statuses.map{|item| item.id}
+		else
+			#前後の月の抽出条件を引き継ぐ egnr=event genres, estt=event statuses
+			_genres = params[:egnr].nil? ? [] : params[:egnr].split(",")
+			_statuses = params[:estt].nil? ? [] : params[:estt].split(",")
+		
+			#フォームでsubmitされたときはフォームの抽出条件で処理する
+			@genre_keys = params[:genre].nil? ? conv_to_i(_genres) : conv_to_i(params[:genre].keys)
+			@status_keys = params[:status].nil? ? conv_to_i(_statuses) : conv_to_i(params[:status].keys)
+		end
     
     @sdate = "#{@year}-#{@month}-01"
     @edate = (Date.new(@year, @month, 1) >> 1).strftime('%Y-%m-%d')
@@ -52,10 +59,10 @@ class PortalCalendar::Public::Node::EventsController < Cms::Controller::Public::
     @items = {}
     @calendar.days.each{|d| @items[d[:date]] = [] if d[:month].to_i == @month }
 
-		events = PortalCalendar::Event.get_period_records_with_content_id(@content.id, @sdate, @edate)
-		events = events.where("genre_id IN (?) AND status_id IN (?)", @genre_keys, @status_keys)
+		@events = PortalCalendar::Event.get_period_records_with_content_id(@content.id, @sdate, @edate)
+		@events = @events.where("genre_id IN (?) AND status_id IN (?)", @genre_keys, @status_keys)
 		
-		events.each do |ev|
+		@events.each do |ev|
       (ev.event_start_date .. ev.event_end_date).each do |evdate|
 				#その月のイベントか？
 				# TODO: 要確認/元の資料のとおりの実装だが、これだとカレンダーに表示される前後の月の範囲のイベントを表示しない仕様。前後の月に移動するとイベントを表示するので違和感あり？
@@ -103,6 +110,7 @@ class PortalCalendar::Public::Node::EventsController < Cms::Controller::Public::
 		end
 		
 		respond_to do |format|
+			format.xml {render :xml => @events}
 			format.html {render :action => "index_calendar"}
 		end
 	end
--- app/controllers/portal_calendar/public/node/lists_controller.rb
+++ app/controllers/portal_calendar/public/node/lists_controller.rb
@@ -11,6 +11,9 @@ class PortalCalendar::Public::Node::ListsController < Cms::Controller::Public::B
 
 		@genres = PortalCalendar::Event.get_genre_valid_list(@content.id)
 		@statuses = PortalCalendar::Event.get_status_valid_list(@content.id)
+
+		#GETで開いたときはページを初期状態で開く
+		@init_page = /GET/ =~ request.headers['REQUEST_METHOD']
   end
 
 	#要素を整数化した配列を返す
@@ -23,13 +26,18 @@ class PortalCalendar::Public::Node::ListsController < Cms::Controller::Public::B
     return http_error(404) if Date.new(@year, @month, 1) < @min_date
     return http_error(404) if Date.new(@year, @month, 1) > @max_date
 
-		#前後の月の抽出条件を引き継ぐ egnr=event genres, estt=event statuses
-		_genres = params[:egnr].nil? ? [] : params[:egnr].split(",")
-		_statuses = params[:estt].nil? ? [] : params[:estt].split(",")
+		if @init_page
+			@genre_keys = @genres.map{|item| item.id}
+			@status_keys = @statuses.map{|item| item.id}
+		else
+			#前後の月の抽出条件を引き継ぐ egnr=event genres, estt=event statuses
+			_genres = params[:egnr].nil? ? [] : params[:egnr].split(",")
+			_statuses = params[:estt].nil? ? [] : params[:estt].split(",")
 		
-		#フォームでsubmitされたときはフォームの抽出条件で処理する
-		@genre_keys = params[:genre].nil? ? conv_to_i(_genres) : conv_to_i(params[:genre].keys)
-		@status_keys = params[:status].nil? ? conv_to_i(_statuses) : conv_to_i(params[:status].keys)
+			#フォームでsubmitされたときはフォームの抽出条件で処理する
+			@genre_keys = params[:genre].nil? ? conv_to_i(_genres) : conv_to_i(params[:genre].keys)
+			@status_keys = params[:status].nil? ? conv_to_i(_statuses) : conv_to_i(params[:status].keys)
+		end
     
     @sdate = "#{@year}-#{@month}-01"
     @edate = (Date.new(@year, @month, 1) >> 1).strftime('%Y-%m-%d')
@@ -40,10 +48,10 @@ class PortalCalendar::Public::Node::ListsController < Cms::Controller::Public::B
     @items = {}
     @calendar.days.each{|d| @items[d[:date]] = [] if d[:month].to_i == @month }
 
-		events = PortalCalendar::Event.get_period_records_with_content_id(@content.id, @sdate, @edate)
-		events = events.where("genre_id IN (?) AND status_id IN (?)", @genre_keys, @status_keys)
+		@events = PortalCalendar::Event.get_period_records_with_content_id(@content.id, @sdate, @edate)
+		@events = @events.where("genre_id IN (?) AND status_id IN (?)", @genre_keys, @status_keys)
 		
-		events.each do |ev|
+		@events.each do |ev|
       (ev.event_start_date .. ev.event_end_date).each do |evdate|
 				#その月のイベントか？
 				# TODO: 要確認/元の資料のとおりの実装だが、これだとカレンダーに表示される前後の月の範囲のイベントを表示しない仕様。前後の月に移動するとイベントを表示するので違和感あり？
@@ -74,6 +82,7 @@ class PortalCalendar::Public::Node::ListsController < Cms::Controller::Public::B
 		prepare_monthly_data
 		
 		respond_to do |format|
+			format.xml  {render :xml => @events}
 			format.html {render :action => "index_monthly"}
 		end
   end
