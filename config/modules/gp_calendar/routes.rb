657cc0d0070ee89f3c0839c479dc2094c70c1b58
zomeki <zomeki@zomeki.jp>
2013-02-04 09:23:33 +0900

    Add least files to show gp_calendar index

--- config/modules/gp_calendar/routes.rb
+++ config/modules/gp_calendar/routes.rb
@@ -5,5 +5,10 @@ ZomekiCMS::Application.routes.draw do
   scope "#{ZomekiCMS::ADMIN_URL_PREFIX}/#{mod}", :module => mod, :as => mod do
     resources :content_base,
       :controller => 'admin/content/base'
+
+    ## contents
+    resources :events,
+      :controller => 'admin/events',
+      :path       => ':content/events'
   end
 end
