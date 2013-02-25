0946ea62e9b4905b3ef49d429186c4afda3f3b4b
zomeki <zomeki@zomeki.jp>
2013-02-25 18:10:14 +0900

    Initialize settings when content creation

--- app/models/gnav/content/menu_item.rb
+++ app/models/gnav/content/menu_item.rb
@@ -4,6 +4,8 @@ class Gnav::Content::MenuItem < Cms::Content
 
   has_many :menu_items, :foreign_key => :content_id, :class_name => 'Gnav::MenuItem', :order => :sort_no, :dependent => :destroy
 
+  before_create :set_default_settings
+
   def menu_item_node
     return @menu_item_node if @menu_item_node
     @menu_item_node = Cms::Node.where(state: 'public', content_id: id, model: 'Gnav::MenuItem').order(:id).first
@@ -23,5 +25,12 @@ class Gnav::Content::MenuItem < Cms::Content
 
   def date_style
     setting_value(:date_style) || ''
+  end
+
+  private
+
+  def set_default_settings
+    in_settings[:list_style] = '@title(@date @group)' unless setting_value(:list_style)
+    in_settings[:date_style] = '%Y年%m月%d日 %H時%M分' unless setting_value(:date_style)
   end
 end
--- app/models/tag/content/tag.rb
+++ app/models/tag/content/tag.rb
@@ -4,6 +4,8 @@ class Tag::Content::Tag < Cms::Content
 
   has_many :tags, :foreign_key => :content_id, :class_name => 'Tag::Tag', :order => 'last_tagged_at DESC', :dependent => :destroy
 
+  before_create :set_default_settings
+
   def tag_node
     return @tag_node if @tag_node
     @tag_node = Cms::Node.where(state: 'public', content_id: id, model: 'Tag::Tag').order(:id).first
@@ -15,5 +17,12 @@ class Tag::Content::Tag < Cms::Content
 
   def date_style
     setting_value(:date_style) || ''
+  end
+
+  private
+
+  def set_default_settings
+    in_settings[:list_style] = '@title(@date @group)' unless setting_value(:list_style)
+    in_settings[:date_style] = '%Y年%m月%d日 %H時%M分' unless setting_value(:date_style)
   end
 end
