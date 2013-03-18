002a0191cfbd5b4db089472c04256a578e9d29e9
Kouichi AZUMA <azuma@idsinc.co.jp>
2013-03-18 19:26:10 +0900

    ピースの表示期間について、18､12､6ヶ月の設定値を用意。 未設定の時は「12ヶ月」を表示する。
    Signed-off-by: Kouichi AZUMA <azuma@idsinc.co.jp>

--- app/models/portal_article/content/setting.rb
+++ app/models/portal_article/content/setting.rb
@@ -20,7 +20,7 @@ class PortalArticle::Content::Setting < Cms::ContentSetting
   set_config :open_fb_comments, :name => 'Facebookコメント表示',
     :options => [['表示する', 'open'], ['表示しない', 'close']]
   set_config :archive_show_terms, :name => 'アーカイブ表示月数',
-    :options => [['12ヶ月', '12'], ['6ヶ月', '6']]
+    :options => [['18ヶ月', '18'], ['12ヶ月', '12'], ['6ヶ月', '6']]
 
   validate :validate_value
   
