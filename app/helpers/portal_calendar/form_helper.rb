dc41eba57b65f4480b024d1663faafbe6474fee8
Kouichi AZUMA <azuma@idsinc.co.jp>
2013-03-19 11:05:06 +0900

    ジャンル表記周りの修正
    Signed-off-by: Kouichi AZUMA <azuma@idsinc.co.jp>

--- app/helpers/portal_calendar/form_helper.rb
+++ app/helpers/portal_calendar/form_helper.rb
@@ -7,11 +7,13 @@ module PortalCalendar::FormHelper
 		content_tag(:ul) do
 			if select_all
 				#全選択オプションの追加
-				concat content_tag(:li, link_to('全選択', url+"?egnr=0&#{param_estt}"))
+				concat content_tag(:li, link_to('全選択', url+"?egnr=0&#{param_estt}"), :class=>'genre01')
 			end
 
+			idx = 2
 			collection.each do |item|
-				concat content_tag(:li, link_to(item.attributes['title'], url+"?egnr=#{item.attributes['id']}&#{param_estt}"))
+				concat content_tag(:li, link_to(item.attributes['title'], url+"?egnr=#{item.attributes['id']}&#{param_estt}"), :class=>sprintf("genre%02d", idx))
+				idx = idx + 1
 			end
 		end
 	end
