df032b7e54914b3d95023adfab0fa66d432bdf53
zomeki <zomeki@zomeki.jp>
2013-03-25 03:41:12 +0900

    Show only public categories on public side

--- app/controllers/gp_category/public/piece/category_types_controller.rb
+++ app/controllers/gp_category/public/piece/category_types_controller.rb
@@ -8,5 +8,15 @@ class GpCategory::Public::Piece::CategoryTypesController < Sys::Controller::Publ
   end
 
   def index
+    case @item
+    when GpCategory::CategoryType
+      @category_type = @item
+      render :category_type
+    when GpCategory::Category
+      @category = @item
+      render :category
+    else
+      @category_types = @piece.public_category_types
+    end
   end
 end
