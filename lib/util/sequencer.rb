17bd32919aab44fa2617f4c995cc24a92ba0e77d
zomeki <zomeki@zomeki.jp>
2013-04-24 01:56:22 +0900

    Refactoring

--- lib/util/sequencer.rb
+++ lib/util/sequencer.rb
@@ -1,29 +1,27 @@
 # encoding: utf-8
+require 'digest/md5'
+
 class Util::Sequencer
   def self.next_id(name, options = {})
     name    = name.to_s
     version = options[:version] || 0
-    
+
     lock = Util::File::Lock.lock("#{name}_#{version}")
-    raise("error: sequencer locked") unless lock
-    
+    raise 'error: sequencer locked' unless lock
+
     if seq = Sys::Sequence.versioned(version.to_s).find_by_name(name)
       seq.value += 1
       seq.save
     else
-      seq = Sys::Sequence.new
-      seq.name = name
-      seq.version = version
-      seq.value = 1
-      seq.save
+      seq = Sys::Sequence.create(name: name, version: version, value: 1)
     end
-    
+
     lock.unlock
-    
+
     if options[:md5]
-      require 'digest/md5'
-      return Digest::MD5.new.update(seq.value.to_s)
+      Digest::MD5.new.update(seq.value.to_s)
+    else
+      seq.value
     end
-    return seq.value
   end
 end
