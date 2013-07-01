bf9322c3e2603bb5b4a00689949d7866e0643fe7
zomeki <zomeki@zomeki.jp>
2013-07-01 15:35:16 +0900

    Erase unused redirection

--- app/controllers/ad_banner/admin/groups_controller.rb
+++ app/controllers/ad_banner/admin/groups_controller.rb
@@ -5,7 +5,6 @@ class AdBanner::Admin::GroupsController < Cms::Controller::Admin::Base
   def pre_dispatch
     return error_auth unless @content = AdBanner::Content::Banner.find_by_id(params[:content])
     return error_auth unless Core.user.has_priv?(:read, :item => @content.concept)
-    return redirect_to(request.env['PATH_INFO']) if params[:reset]
   end
 
   def index
--- app/controllers/map/admin/markers_controller.rb
+++ app/controllers/map/admin/markers_controller.rb
@@ -5,7 +5,6 @@ class Map::Admin::MarkersController < Cms::Controller::Admin::Base
   def pre_dispatch
     return error_auth unless @content = Map::Content::Marker.find_by_id(params[:content])
     return error_auth unless Core.user.has_priv?(:read, :item => @content.concept)
-    return redirect_to(request.env['PATH_INFO']) if params[:reset]
   end
 
   def index
