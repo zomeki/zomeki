759d0e6fa37f052a0fd040d807e6d4844a890e19
zomeki <zomeki@zomeki.jp>
2013-07-04 21:45:36 +0900

    Add gp_calender events without images

---
+++ app/models/gp_calendar/event.rb
@@ -0,0 +1,48 @@
+# encoding: utf-8
+class GpCalendar::Event < ActiveRecord::Base
+  include Sys::Model::Base
+  include Sys::Model::Rel::Creator
+  include Cms::Model::Auth::Content
+
+  STATE_OPTIONS = [['公開', 'public'], ['非公開', 'closed']]
+  TARGET_OPTIONS = [['同一ウィンドウ', '_self'], ['別ウィンドウ', '_blank']]
+  ORDER_OPTIONS = [['作成日時（降順）', 'created_at_desc'], ['作成日時（昇順）', 'created_at_asc']]
+
+  # Content
+  belongs_to :content, :foreign_key => :content_id, :class_name => 'GpCalendar::Content::Event'
+  validates_presence_of :content_id
+
+  # Proper
+  belongs_to :status, :foreign_key => :state, :class_name => 'Sys::Base::Status'
+  validates_presence_of :state
+
+  validates_presence_of :started_on, :ended_on, :name
+
+  after_initialize :set_defaults
+
+  def self.all_with_content_and_criteria(content, criteria)
+    events = self.arel_table
+
+    rel = self.where(events[:content_id].eq(content.id))
+    rel = rel.where(events[:name].matches("%#{criteria[:name]}%")) if criteria[:name].present?
+    rel = rel.where(events[:started_on].lteq(criteria[:date])
+                    .and(events[:ended_on].gteq(criteria[:date]))) if criteria[:date].present?
+    rel = case criteria[:order]
+          when 'created_at_desc'
+            rel.except(:order).order(events[:created_at].desc)
+          when 'created_at_asc'
+            rel.except(:order).order(events[:created_at].asc)
+          else
+            rel
+          end
+
+    return rel
+  end
+
+  private
+
+  def set_defaults
+    self.state ||= STATE_OPTIONS.first.last if self.has_attribute?(:state)
+    self.target ||= TARGET_OPTIONS.first.last if self.has_attribute?(:target)
+  end
+end
