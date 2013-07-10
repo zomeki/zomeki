24ba86d693df2f7071c687786385aa09b578b84c
zomeki <zomeki@zomeki.jp>
2013-07-10 11:56:06 +0900

    Rename library

--- public/_common/js/simple_tabs.js
+++ public/_common/js/simple_tabs.js
@@ -1,12 +1,12 @@
 function enable_simple_tabs() {
   var simple_tabs = $('#simple_tabs > li');
-  var simple_tab_contents = $('#simple_tab_contents > div');
+  var simple_tab_panels = $('#simple_tab_panels > div');
 
   var index;
   simple_tabs.on('click', function () {
     if (index != simple_tabs.index(this)) {
       index = simple_tabs.index(this);
-      simple_tab_contents.hide().eq(index).fadeIn('fast');
+      simple_tab_panels.hide().eq(index).fadeIn('fast');
       simple_tabs.removeClass('current').eq(index).addClass('current');
 
       if (me) {
