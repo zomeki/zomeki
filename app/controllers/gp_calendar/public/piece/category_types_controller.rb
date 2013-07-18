0fd0f06198ea7f5fb270867cb819245323d41eb7
zomeki <zomeki@zomeki.jp>
2013-07-18 23:56:06 +0900

    Just refactoring

--- app/controllers/gp_calendar/public/piece/category_types_controller.rb
+++ app/controllers/gp_calendar/public/piece/category_types_controller.rb
@@ -2,9 +2,9 @@
 class GpCalendar::Public::Piece::CategoryTypesController < Sys::Controller::Public::Base
   def pre_dispatch
     @piece = GpCalendar::Piece::CategoryType.find_by_id(Page.current_piece.id)
-    render :text => '' unless @piece
+    return render(:text => '') unless @piece
     @content = GpCalendar::Content::Event.find_by_id(@piece.content.id)
-    render :text => '' unless @content
+    return render(:text => '') unless @content
 
     @item = Page.current_item
   end
