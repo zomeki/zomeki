569274dd1d5034e1c0c34efa30887aef65cf229c
zomeki <zomeki@zomeki.jp>
2013-07-20 02:08:14 +0900

    Now get content via piece

--- app/controllers/gp_calendar/public/piece/category_types_controller.rb
+++ app/controllers/gp_calendar/public/piece/category_types_controller.rb
@@ -3,8 +3,6 @@ class GpCalendar::Public::Piece::CategoryTypesController < Sys::Controller::Publ
   def pre_dispatch
     @piece = GpCalendar::Piece::CategoryType.find_by_id(Page.current_piece.id)
     return render(:text => '') unless @piece
-    @content = GpCalendar::Content::Event.find_by_id(@piece.content.id)
-    return render(:text => '') unless @content
 
     @item = Page.current_item
   end
