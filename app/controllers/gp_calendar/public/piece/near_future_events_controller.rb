318d754fa77faac3a447e05d1711b39a4178ffa5
zomeki <zomeki@zomeki.jp>
2013-07-20 02:08:14 +0900

    Now get content via piece

--- app/controllers/gp_calendar/public/piece/near_future_events_controller.rb
+++ app/controllers/gp_calendar/public/piece/near_future_events_controller.rb
@@ -3,15 +3,13 @@ class GpCalendar::Public::Piece::NearFutureEventsController < Sys::Controller::P
   def pre_dispatch
     @piece = GpCalendar::Piece::NearFutureEvent.find_by_id(Page.current_piece.id)
     return render(:text => '') unless @piece
-    @content = GpCalendar::Content::Event.find_by_id(@piece.content.id)
-    return render(:text => '') unless @content
 
     @item = Page.current_item
   end
 
   def index
     today = Date.today
-    @todays_events = GpCalendar::Event.public.all_with_content_and_criteria(@content, date: today)
-    @tomorrows_events = GpCalendar::Event.public.all_with_content_and_criteria(@content, date: today.tomorrow)
+    @todays_events = GpCalendar::Event.public.all_with_content_and_criteria(@piece.content, date: today)
+    @tomorrows_events = GpCalendar::Event.public.all_with_content_and_criteria(@piece.content, date: today.tomorrow)
   end
 end
--- app/models/gp_calendar/piece/category_type.rb
+++ app/models/gp_calendar/piece/category_type.rb
@@ -1,4 +1,8 @@
 # encoding: utf-8
 class GpCalendar::Piece::CategoryType < Cms::Piece
   default_scope where(model: 'GpCalendar::CategoryType')
+
+  def content
+    GpCalendar::Content::Event.find(super)
+  end
 end
