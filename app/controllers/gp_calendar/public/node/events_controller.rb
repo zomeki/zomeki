d6026ed7dc53d5698e08d3c57885c835372d3944
zomeki <zomeki@zomeki.jp>
2013-07-21 01:16:51 +0900

    Now show docs to events page

--- app/controllers/gp_calendar/public/node/events_controller.rb
+++ app/controllers/gp_calendar/public/node/events_controller.rb
@@ -22,6 +22,7 @@ class GpCalendar::Public::Node::EventsController < Cms::Controller::Public::Base
   def index
     criteria = {month: @date.strftime('%Y%m')}
     @events = GpCalendar::Event.public.all_with_content_and_criteria(@content, criteria).order(:started_on)
+    merge_docs_into_events(event_docs(@date.beginning_of_month, @date.end_of_month), @events)
   end
 
   # OBSOLETED
@@ -114,20 +115,30 @@ class GpCalendar::Public::Node::EventsController < Cms::Controller::Public::Base
     return @date.between?(@min_date, @max_date)
   end
 
-  def event_docs(sdate, edate)
+  def event_docs(start_date, end_date)
     doc_contents = Cms::ContentSetting.where(name: 'gp_calendar_content_event_id', value: @content.id).map(&:content)
-    doc_contents.reject! {|dc| dc.site != Page.site }
-    return doc_contents if doc_contents.empty?
+    doc_contents.select! {|dc| dc.site == Page.site }
+    return [] if doc_contents.empty?
 
     doc_contents.map {|dc|
       case dc.model
       when 'GpArticle::Doc'
         dc = GpArticle::Content::Doc.find(dc.id)
         docs = dc.public_docs.table
-        dc.public_docs.where(event_state: 'visible').where(docs[:event_date].gteq(sdate).and(docs[:event_date].lt(edate)))
+        dc.public_docs.where(event_state: 'visible').where(docs[:event_date].gteq(start_date).and(docs[:event_date].lteq(end_date)))
       else
         []
       end
     }.flatten
+  end
+
+  def merge_docs_into_events(docs, events)
+    docs.each do |doc|
+      event = GpCalendar::Event.new(title: doc.title, href: doc.public_uri, target: '_self',
+                                    started_on: doc.event_date, ended_on: doc.event_date, description: doc.summary)
+      event.files = doc.files
+      events << event
+    end
+    events.sort! {|a, b| a.started_on <=> b.started_on }
   end
 end
