54e23f7868a1aba9c47d9fbb3b84c61b21168d2a
zomeki <zomeki@zomeki.jp>
2013-07-21 00:26:31 +0900

    Show docs to near_future_event piece

--- app/controllers/gp_calendar/public/piece/near_future_events_controller.rb
+++ app/controllers/gp_calendar/public/piece/near_future_events_controller.rb
@@ -11,5 +11,37 @@ class GpCalendar::Public::Piece::NearFutureEventsController < Sys::Controller::P
     today = Date.today
     @todays_events = GpCalendar::Event.public.all_with_content_and_criteria(@piece.content, date: today)
     @tomorrows_events = GpCalendar::Event.public.all_with_content_and_criteria(@piece.content, date: today.tomorrow)
+
+    merge_docs_into_events(event_docs(today, today), @todays_events)
+    merge_docs_into_events(event_docs(today.tomorrow, today.tomorrow), @tomorrows_events)
+  end
+
+  private
+
+  def event_docs(start_date, end_date)
+    doc_contents = Cms::ContentSetting.where(name: 'gp_calendar_content_event_id', value: @piece.content.id).map(&:content)
+    doc_contents.select! {|dc| dc.site == Page.site }
+    return [] if doc_contents.empty?
+
+    doc_contents.map {|dc|
+      case dc.model
+      when 'GpArticle::Doc'
+        dc = GpArticle::Content::Doc.find(dc.id)
+        docs = dc.public_docs.table
+        dc.public_docs.where(event_state: 'visible').where(docs[:event_date].gteq(start_date).and(docs[:event_date].lteq(end_date)))
+      else
+        []
+      end
+    }.flatten
+  end
+
+  def merge_docs_into_events(docs, events)
+    docs.each do |doc|
+      event = GpCalendar::Event.new(title: doc.title, href: doc.public_uri, target: '_self',
+                                    started_on: doc.event_date, ended_on: doc.event_date, description: doc.summary)
+      event.files = doc.files
+      events << event
+    end
+    events.sort! {|a, b| a.started_on <=> b.started_on }
   end
 end
