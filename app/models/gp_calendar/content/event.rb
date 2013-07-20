3d7fe201c086817f64abe3b3ddc1e4b5d2cb5a66
zomeki <zomeki@zomeki.jp>
2013-07-21 01:06:35 +0900

    Now show docs to today's event page

--- app/models/gp_calendar/content/event.rb
+++ app/models/gp_calendar/content/event.rb
@@ -6,6 +6,10 @@ class GpCalendar::Content::Event < Cms::Content
 
   before_create :set_default_settings
 
+  def public_node
+    Cms::Node.where(state: 'public', content_id: id, model: 'GpCalendar::Event').order(:id).first
+  end
+
   def event_node
     return @event_node if @event_node
     @event_node = Cms::Node.where(state: 'public', content_id: id, model: 'GpCalendar::Event').order(:id).first
