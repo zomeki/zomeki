104bbe137aa7b528f58609de3a95a4188c58707f
zomeki <zomeki@zomeki.jp>
2013-07-23 09:59:56 +0900

    Add marker image on public side

--- app/controllers/map/public/node/markers_controller.rb
+++ app/controllers/map/public/node/markers_controller.rb
@@ -3,6 +3,8 @@
 require 'will_paginate/array'
 
 class Map::Public::Node::MarkersController < Cms::Controller::Public::Base
+  skip_filter :render_public_layout, :only => [:file_content]
+
   def pre_dispatch
     @node = Page.current_node
     @content = Map::Content::Marker.find_by_id(@node.content.id)
@@ -18,6 +20,17 @@ class Map::Public::Node::MarkersController < Cms::Controller::Public::Base
       markers = @content.public_markers + public_doc_markers
     end
     @markers = markers.paginate(page: params[:page], per_page: 30)
+  end
+
+  def file_content
+    @marker = @content.markers.find_by_name(params[:name])
+    return http_error(404) if @marker.nil? || @marker.files.empty?
+
+    file = @marker.files.first
+    mt = file.mime_type.presence || Rack::Mime.mime_type(File.extname(file.name))
+    type, disposition = (mt =~ %r!^image/|^application/pdf$! ? [mt, 'inline'] : [mt, 'attachment'])
+    disposition = 'attachment' if request.env['HTTP_USER_AGENT'] =~ /Android/
+    send_file file.upload_path, :type => type, :filename => file.name, :disposition => disposition
   end
 
   private
