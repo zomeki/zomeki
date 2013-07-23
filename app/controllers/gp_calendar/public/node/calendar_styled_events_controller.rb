6d58d872f4943640f7eccd2f26634a7aae0fe4b9
zomeki <zomeki@zomeki.jp>
2013-07-23 11:47:05 +0900

    Refactoring

--- app/controllers/gp_calendar/public/node/calendar_styled_events_controller.rb
+++ app/controllers/gp_calendar/public/node/calendar_styled_events_controller.rb
@@ -1,20 +1,8 @@
 # encoding: utf-8
-class GpCalendar::Public::Node::CalendarStyledEventsController < Cms::Controller::Public::Base
-  def pre_dispatch
-    @node = Page.current_node
-    @content = GpCalendar::Content::Event.find_by_id(@node.content.id)
-    return http_error(404) unless @content
-
-    @today = Date.today
-    @min_date = @today.beginning_of_month
-    @max_date = 11.months.since(@min_date)
-
-    return http_error(404) unless validate_date
-  end
-
+class GpCalendar::Public::Node::CalendarStyledEventsController < GpCalendar::Public::Node::BaseController
   def index
     criteria = {month: @date.strftime('%Y%m')}
-    @events = GpCalendar::Event.public.all_with_content_and_criteria(@content, criteria)
+    @events = GpCalendar::Event.public.all_with_content_and_criteria(@content, criteria).order(:started_on)
 
     start_date = @date.beginning_of_month.beginning_of_week(:sunday)
     end_date = @date.end_of_month.end_of_week(:sunday)
@@ -24,20 +12,5 @@ class GpCalendar::Public::Node::CalendarStyledEventsController < Cms::Controller
         weeks.last.push(day)
         next weeks
       end
-  end
-
-  private
-
-  def validate_date
-    @month = params[:month].to_i
-    @month = @today.month if @month.zero?
-    return false unless @month.between?(1, 12)
-
-    @year = params[:year].to_i
-    @year = @today.year if @year.zero?
-    return false unless @year.between?(1900, 2100)
-
-    @date = Date.new(@year, @month, 1)
-    return @date.between?(@min_date, @max_date)
   end
 end
--- app/controllers/gp_calendar/public/node/todays_events_controller.rb
+++ app/controllers/gp_calendar/public/node/todays_events_controller.rb
@@ -1,57 +1,13 @@
 # encoding: utf-8
-class GpCalendar::Public::Node::TodaysEventsController < Cms::Controller::Public::Base
-  def pre_dispatch
-    @node = Page.current_node
-    @content = GpCalendar::Content::Event.find_by_id(@node.content.id)
-    return http_error(404) unless @content
-  end
-
+class GpCalendar::Public::Node::TodaysEventsController < GpCalendar::Public::Node::BaseController
   def index
-    @today = Date.today
     criteria = {date: @today}
-    @events = GpCalendar::Event.public.all_with_content_and_criteria(@content, criteria)
+    @events = GpCalendar::Event.public.all_with_content_and_criteria(@content, criteria).order(:started_on)
 
     merge_docs_into_events(event_docs(@today, @today), @events)
 
     if (category = find_category_by_specified_path(params[:category]))
       @events.select! {|e| e.category_ids.include?(category.id) }
     end
-  end
-
-  private
-
-  def find_category_by_specified_path(path)
-    return nil unless path.kind_of?(String)
-    category_type_name, category_path = path.split('/', 2)
-    category_type = @content.category_types.find_by_name(category_type_name)
-    return nil unless category_type
-    category_type.find_category_by_path_from_root_category(category_path)
-  end
-
-  def event_docs(start_date, end_date)
-    doc_contents = Cms::ContentSetting.where(name: 'gp_calendar_content_event_id', value: @content.id).map(&:content)
-    doc_contents.select! {|dc| dc.site == Page.site }
-    return [] if doc_contents.empty?
-
-    doc_contents.map {|dc|
-      case dc.model
-      when 'GpArticle::Doc'
-        dc = GpArticle::Content::Doc.find(dc.id)
-        docs = dc.public_docs.table
-        dc.public_docs.where(event_state: 'visible').where(docs[:event_date].gteq(start_date).and(docs[:event_date].lteq(end_date)))
-      else
-        []
-      end
-    }.flatten
-  end
-
-  def merge_docs_into_events(docs, events)
-    docs.each do |doc|
-      event = GpCalendar::Event.new(title: doc.title, href: doc.public_uri, target: '_self',
-                                    started_on: doc.event_date, ended_on: doc.event_date, description: doc.summary)
-      event.files = doc.files
-      events << event
-    end
-    events.sort! {|a, b| a.started_on <=> b.started_on }
   end
 end
