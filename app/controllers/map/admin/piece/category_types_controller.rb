e440fd43604a850f6080f2e3e21b0b6c6015f555
zomeki <zomeki@zomeki.jp>
2013-07-24 14:50:34 +0900

    Add visible_category_types setting to piece

--- app/controllers/map/admin/piece/category_types_controller.rb
+++ app/controllers/map/admin/piece/category_types_controller.rb
@@ -1,2 +1,17 @@
 class Map::Admin::Piece::CategoryTypesController < Cms::Admin::Piece::BaseController
+  def update
+    item_in_settings = (params[:item][:in_settings] || {})
+    if (ids = params[:category_types]).is_a?(Array)
+      category_type_ids = ids.map{|id| id.to_i if id.present? }.compact.uniq
+      item_in_settings[:category_type_ids] = YAML.dump(category_type_ids)
+    end
+    params[:item][:in_settings] = item_in_settings
+    super
+  end
+
+  private
+
+  def find_piece
+    model.new.readable.find(params[:id])
+  end
 end
