1186eca42277e8b6e8c6b948ec4dfe98d9916b19
zomeki <zomeki@zomeki.jp>
2013-07-26 12:33:02 +0900

    Now require "factory_girl_rails" manually

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'factory_girl_rails'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
