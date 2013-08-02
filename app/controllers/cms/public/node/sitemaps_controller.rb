ab4a136b0af3783d61587438d597de5fedb91323
zomeki <zomeki@zomeki.jp>
2013-08-02 21:01:55 +0900

    Now show NOT directory also on root

--- app/controllers/cms/public/node/sitemaps_controller.rb
+++ app/controllers/cms/public/node/sitemaps_controller.rb
@@ -8,17 +8,16 @@ class Cms::Public::Node::SitemapsController < Cms::Controller::Public::Base
 
     item = Cms::Node.new.public
     item.and :route_id, Page.site.root_node.id
-    item.and :directory, 1
     item.and :name, 'IS NOT', nil
     item.and :sitemap_state, 'visible'
-    @items = item.find(:all, :order => 'sitemap_sort_no IS NULL, sitemap_sort_no, name')
+    @items = item.find(:all, :order => 'directory DESC, sitemap_sort_no IS NULL, sitemap_sort_no, name')
 
     @children = lambda do |node|
       item = Cms::Node.new.public
       item.and :route_id, node.id
       item.and :name, 'IS NOT', nil
       item.and :sitemap_state, 'visible'
-      item.find(:all, :order => 'directory, sitemap_sort_no IS NULL, sitemap_sort_no, name')
+      item.find(:all, :order => 'directory DESC, sitemap_sort_no IS NULL, sitemap_sort_no, name')
     end
   end
 end
