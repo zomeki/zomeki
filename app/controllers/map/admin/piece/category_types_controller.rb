bd8ae49689f3a994351184e7b12f80a620c13546
zomeki <zomeki@zomeki.jp>
2013-08-05 10:54:24 +0900

    Add target_node to piece options

--- app/controllers/map/admin/piece/category_types_controller.rb
+++ app/controllers/map/admin/piece/category_types_controller.rb
@@ -1,10 +1,13 @@
 class Map::Admin::Piece::CategoryTypesController < Cms::Admin::Piece::BaseController
   def update
     item_in_settings = (params[:item][:in_settings] || {})
+
     if (ids = params[:category_types]).is_a?(Array)
       category_type_ids = ids.map{|id| id.to_i if id.present? }.compact.uniq
       item_in_settings[:category_type_ids] = YAML.dump(category_type_ids)
     end
+    item_in_settings[:target_node_id] = params[:target_node]
+
     params[:item][:in_settings] = item_in_settings
     super
   end
