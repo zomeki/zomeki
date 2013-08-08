db61ff4e43f3e9acbc2a1bef50c9f995e6b42a16
zomeki <zomeki@zomeki.jp>
2013-08-08 19:03:01 +0900

    Change comment

--- app/models/map/content/setting.rb
+++ app/models/map/content/setting.rb
@@ -3,7 +3,7 @@ class Map::Content::Setting < Cms::ContentSetting
   set_config :gp_category_content_category_type_id, :name => '汎用カテゴリタイプ',
     :options => lambda { GpCategory::Content::CategoryType.all.map {|ct| [ct.name, ct.id] } }
   set_config :lat_lng, :name => '地図/デフォルト座標',
-    :comment => '緯度,経度'
+    :comment => '（緯度,経度）'
   set_config :show_images, :name => '画像表示',
     :options => Map::Marker::IMAGE_STATE_OPTIONS,
     :form_type => :radio_buttons
