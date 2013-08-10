b554318e3e11197982b5e7ab72deb8e8b7711538
zomeki <zomeki@zomeki.jp>
2013-08-10 16:00:10 +0900

    Now only events and calendar styled events are available for daily links piece

--- app/helpers/gp_calendar/gp_calendar_helper.rb
+++ app/helpers/gp_calendar/gp_calendar_helper.rb
@@ -3,4 +3,8 @@ module GpCalendar::GpCalendarHelper
   def localize_wday(style, wday)
     style.gsub('%A', t('date.day_names')[wday]).gsub('%a', t('date.abbr_day_names')[wday])
   end
+
+  def nodes_for_daily_links(nodes)
+    nodes.select {|n| %w!GpCalendar::Event GpCalendar::CalendarStyledEvent!.include?(n.model) }
+  end
 end
