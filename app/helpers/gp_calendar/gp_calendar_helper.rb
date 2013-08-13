6e605624d341a222a3548d54aea9e1d273af6f76
zomeki <zomeki@zomeki.jp>
2013-08-13 10:36:09 +0900

    Now only events/calendar_styled_events are available to category_types piece

--- app/helpers/gp_calendar/gp_calendar_helper.rb
+++ app/helpers/gp_calendar/gp_calendar_helper.rb
@@ -4,12 +4,16 @@ module GpCalendar::GpCalendarHelper
     style.gsub('%A', t('date.day_names')[wday]).gsub('%a', t('date.abbr_day_names')[wday])
   end
 
-  def nodes_for_daily_links(nodes)
+  def nodes_for_category_types(nodes)
     nodes.select {|n| %w!GpCalendar::Event
                          GpCalendar::CalendarStyledEvent!.include?(n.model) }
   end
 
+  def nodes_for_daily_links(nodes)
+    nodes_for_category_types(nodes)
+  end
+
   def nodes_for_monthly_links(nodes)
-    nodes_for_daily_links(nodes)
+    nodes_for_category_types(nodes)
   end
 end
