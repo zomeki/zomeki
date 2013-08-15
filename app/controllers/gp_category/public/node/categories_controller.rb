b6acbb08f8e8a1e37163b9b451203697ada5a2e7
zomeki <zomeki@zomeki.jp>
2013-08-15 14:59:38 +0900

    Apply category doc style

--- app/controllers/gp_category/public/node/categories_controller.rb
+++ app/controllers/gp_category/public/node/categories_controller.rb
@@ -13,11 +13,15 @@ class GpCategory::Public::Node::CategoriesController < Cms::Controller::Public::
     Page.current_item = @category
     Page.title = @category.title
 
-    @children = @category.public_children
-
     @docs = @category.public_docs.paginate(page: params[:page], per_page: 20)
     return http_error(404) if @docs.current_page > @docs.total_pages
 
-    render :show_mobile if Page.mobile?
+    if Page.mobile?
+      render :show_mobile
+    else
+      if (style = @content.category_style).present?
+        render style
+      end
+    end
   end
 end
