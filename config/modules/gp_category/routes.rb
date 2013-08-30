df23e3313dd30c0bd3c01a1894c2134ff59efba4
zomeki <zomeki@zomeki.jp>
2013-08-30 09:32:14 +0900

    Add "more" link to category page

--- config/modules/gp_category/routes.rb
+++ config/modules/gp_category/routes.rb
@@ -46,10 +46,10 @@ ZomekiCMS::Application.routes.draw do
 
   ## public
   scope "_public/#{mod}", :module => mod, :as => '' do
-    match 'node_category_types(/index.:format)' => 'public/node/category_types#index'
-    match 'node_category_types/:name(/index.:format)' => 'public/node/category_types#show'
-    match 'node_category_types/:category_type_name/*category_names/index(.:format)' => 'public/node/categories#show'
+    match 'node_category_types(/index)' => 'public/node/category_types#index'
+    match 'node_category_types/:name(/index)' => 'public/node/category_types#show'
+    match 'node_category_types/:category_type_name/*category_names/:file' => 'public/node/categories#show'
     match 'node_category_types/:category_type_name/*category_names' => 'public/node/categories#show'
-    match 'node_docs(/index.:format)' => 'public/node/docs#index'
+    match 'node_docs(/index)' => 'public/node/docs#index'
   end
 end
