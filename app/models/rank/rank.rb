bbb93b6e335a2cb3effa23b796a9f38fceb157c0
Masaki Ozawa <ozamasa123@gmail.com>
2013-08-30 14:22:07 +0900

    Add search option to access ranking page

--- app/models/rank/rank.rb
+++ app/models/rank/rank.rb
@@ -3,6 +3,6 @@ class Rank::Rank < ActiveRecord::Base
 
   # Content
   belongs_to :content, :foreign_key => :content_id, :class_name => 'Rank::Content::Rank'
-#  validates_presence_of :content_id
+  validates_presence_of :content_id
 
 end
