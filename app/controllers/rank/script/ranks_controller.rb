ee2257c5086b14cea5d5ebb0cbba5c9da5070e4d
zomeki <zomeki@zomeki.jp>
2013-09-04 17:42:08 +0900

    Fix file uploading

--- app/controllers/rank/script/ranks_controller.rb
+++ app/controllers/rank/script/ranks_controller.rb
@@ -4,7 +4,7 @@ class Rank::Script::RanksController < ApplicationController
   include Rank::Controller::Rank
 
   def exec
-    span = 10.days
+    span = 3.days
     contents = Rank::Content::Rank.all
     contents.each do |content|
       start_date = DateTime.parse(content.setting_value(:start_date)) rescue Time.now - span
