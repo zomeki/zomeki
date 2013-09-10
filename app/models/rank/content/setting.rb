e6ec15e92244dae185e19056a5570f828bc9240e
Masaki Ozawa <ozamasa123@gmail.com>
2013-09-10 11:05:22 +0900

    Add display setting to access ranking

--- app/models/rank/content/setting.rb
+++ app/models/rank/content/setting.rb
@@ -8,4 +8,6 @@ class Rank::Content::Setting < Cms::ContentSetting
     :comment => '例：UA-33912981-1'
   set_config :start_date, :name => '期間指定',
     :form_type => :date, :comment => ''
+  set_config :show_count, :name => "アクセス数の表示",
+    :options => [["表示する", 1], ["表示しない", 0]]
 end
