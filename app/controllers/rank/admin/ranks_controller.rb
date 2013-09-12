00c48de0802708c810f4290442e4eb593806ced7
Masaki Ozawa <ozamasa123@gmail.com>
2013-09-12 11:21:01 +0900

    Add paginate view to access ranking

--- app/controllers/rank/admin/ranks_controller.rb
+++ app/controllers/rank/admin/ranks_controller.rb
@@ -12,17 +12,19 @@ class Rank::Admin::RanksController < Cms::Controller::Admin::Base
 
   def index
     if @content.ranks.empty?
-      start_date = (DateTime.parse(@content.setting_value(:start_date)) rescue nil) || Time.now - 1.month
+      start_date = (DateTime.parse(@content.setting_value(:start_date)) rescue nil)
 
       get_access(@content, start_date)
     end
+
+    per_page   = 20
 
     rank_table = Rank::Rank.arel_table
     @target = params[:target]
     @target = 'pageviews' unless @target == 'pageviews' || @target == 'visitors'
     from    = params[:from].presence || '2005-01-01'
     to      = params[:to].presence   || Date.today.strftime('%F')
-    @ranks  = @content.ranks.where(rank_table[:date].gteq(from).and(rank_table[:date].lteq(to))).select('*').select(rank_table[@target].sum.as('accesses')).group(rank_table[:page_path]).order('accesses DESC').paginate(page: params[:page], per_page: 50)
+    @ranks  = @content.ranks.where(rank_table[:date].gteq(from).and(rank_table[:date].lteq(to))).select('*').select(rank_table[@target].sum.as('accesses')).group(rank_table[:page_path]).order('accesses DESC').paginate(page: params[:page], per_page: per_page)
 
     _index @ranks
   end
