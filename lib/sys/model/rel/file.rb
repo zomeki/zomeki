91243f311802b1ed16c30d73033bdb61e81b5403
Masaki Ozawa <ozamasa123@gmail.com>
2013-10-18 00:15:50 +0900

    Fix map & gp_calendar

--- lib/sys/model/rel/file.rb
+++ lib/sys/model/rel/file.rb
@@ -3,28 +3,28 @@ module Sys::Model::Rel::File
   def self.included(mod)
     mod.has_many :files, :foreign_key => 'parent_unid', :class_name => 'Sys::File',
       :primary_key => 'unid', :dependent => :destroy
-    
+
     mod.before_save :publish_files
     mod.before_save :close_files
   end
-  
+
   ## Remove the temporary flag.
   def fix_tmp_files(tmp_id)
     Sys::File.fix_tmp_files(tmp_id, unid)
     return true
   end
-  
+
   def public_files_path
     "#{::File.dirname(public_path)}/files"
   end
-  
+
   def publish_files
     return true unless @save_mode == :publish
     return true if files.size == 0
-    
+
     dir = public_files_path
     FileUtils.mkdir_p(dir) unless FileTest.exist?(dir)
-    
+
     files.each do |file|
       next unless FileTest.exist?(file.upload_path)
       new_file = dir + '/' + file.name
@@ -37,10 +37,10 @@ module Sys::Model::Rel::File
     end
     return true
   end
-  
+
   def close_files
     return true unless @save_mode == :close
-    
+
     dir = public_files_path
     FileUtils.rm_r(dir) if FileTest.exist?(dir)
     return true
@@ -48,5 +48,15 @@ module Sys::Model::Rel::File
 
   def image_files
     files.select {|f| f.image_file? }
+  end
+
+  def insert_files
+    inserts = []
+    if doc.present?
+      doc.body.scan(/file_contents\/(\S+)"/).each do |iname|
+        inserts.concat files.select {|f| f.image_file? && f.name == iname[0].to_s }
+      end
+    end
+    return inserts.uniq
   end
 end
