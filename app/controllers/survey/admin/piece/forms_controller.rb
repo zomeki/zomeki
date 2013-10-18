4d386407d4f1febaabfa9560e604a63705cf9a4e
zomeki <zomeki@zomeki.jp>
2013-10-18 14:52:22 +0900

    Add upper_text/lower_text to piece

--- app/controllers/survey/admin/piece/forms_controller.rb
+++ app/controllers/survey/admin/piece/forms_controller.rb
@@ -3,6 +3,8 @@ class Survey::Admin::Piece::FormsController < Cms::Admin::Piece::BaseController
     item_in_settings = (params[:item][:in_settings] || {})
 
     item_in_settings[:target_form_id] = params[:target_form]
+    item_in_settings[:upper_text] = params[:upper_text]
+    item_in_settings[:lower_text] = params[:lower_text]
 
     params[:item][:in_settings] = item_in_settings
     super
