9e4ff246888192b4b731564bbf8d1b74e91d4792
zomeki <zomeki@zomeki.jp>
2013-10-22 20:37:07 +0900

    Add csv download answers

--- app/controllers/survey/admin/forms_controller.rb
+++ app/controllers/survey/admin/forms_controller.rb
@@ -1,4 +1,7 @@
 # encoding: utf-8
+
+require 'csv'
+
 class Survey::Admin::FormsController < Cms::Controller::Admin::Base
   include Sys::Controller::Scaffold::Base
 
@@ -35,5 +38,36 @@ class Survey::Admin::FormsController < Cms::Controller::Admin::Base
   def destroy
     @item = @content.forms.find(params[:id])
     _destroy @item
+  end
+
+  def download_form_answers
+    @item = @content.forms.find(params[:id])
+
+    csv_string = CSV.generate do |csv|
+      header = [Survey::FormAnswer.human_attribute_name(:created_at),
+                "#{Survey::FormAnswer.human_attribute_name(:answered_url)}URL",
+                "#{Survey::FormAnswer.human_attribute_name(:answered_url)}タイトル",
+                Survey::FormAnswer.human_attribute_name(:remote_addr),
+                Survey::FormAnswer.human_attribute_name(:user_agent)]
+
+      @item.questions.each{|q| header << q.title }
+
+      csv << header
+
+      @item.form_answers.each do |form_answer|
+        line = [I18n.l(form_answer.created_at),
+                form_answer.answered_url,
+                form_answer.answered_url_title,
+                form_answer.remote_addr,
+                form_answer.user_agent]
+
+        @item.questions.each{|q| line << form_answer.answers.find_by_question_id(q.id).try(:content) }
+
+        csv << line
+      end
+    end
+
+    send_data csv_string.encode(Encoding::WINDOWS_31J, :invalid => :replace, :undef => :replace),
+              type: Rack::Mime.mime_type('.csv'), filename: 'answers.csv'
   end
 end
