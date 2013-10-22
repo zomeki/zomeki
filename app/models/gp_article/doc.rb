9b542729ef720b296ebaaf6d0b0838d6d40776b6
zomeki <zomeki@zomeki.jp>
2013-10-22 16:07:49 +0900

    Now approved notification mail sender is last approver

--- app/models/gp_article/doc.rb
+++ app/models/gp_article/doc.rb
@@ -435,7 +435,7 @@ class GpArticle::Doc < ActiveRecord::Base
     #{content.site.full_uri.sub(/\/+$/, '')}#{Rails.application.routes.url_helpers.gp_article_doc_path(content: content, id: id)}
       EOT
 
-      approval_request.current_assignments.map{|a| a.user unless a.state == 'approved' }.compact.each do |approver|
+      approval_request.current_assignments.map{|a| a.user unless a.approved_at }.compact.each do |approver|
         next if approval_request.user.email.blank? || approver.email.blank?
         CommonMailer.plain(from: approval_request.user.email, to: approver.email, subject: subject, body: body).deliver
       end
@@ -454,13 +454,14 @@ class GpArticle::Doc < ActiveRecord::Base
   #{content.site.full_uri.sub(/\/+$/, '')}#{Rails.application.routes.url_helpers.gp_article_doc_path(content: content, id: id)}
       EOT
 
-      next if Core.user.email.blank? || approval_request.user.email.blank?
-      CommonMailer.plain(from: Core.user.email, to: approval_request.user.email, subject: subject, body: body).deliver
+      approver = approval_request.current_assignments.reorder('approved_at DESC').first.user
+      next if approver.email.blank? || approval_request.user.email.blank?
+      CommonMailer.plain(from: approver.email, to: approval_request.user.email, subject: subject, body: body).deliver
     end
   end
 
   def approvers
-    approval_requests.inject([]){|u, r| u | r.current_assignments.map{|a| a.user unless a.state == 'approved' }.compact }
+    approval_requests.inject([]){|u, r| u | r.current_assignments.map{|a| a.user unless a.approved_at }.compact }
   end
 
   def approve(user)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131020235113) do
+ActiveRecord::Schema.define(:version => 20131022065758) do
 
   create_table "ad_banner_banners", :force => true do |t|
     t.string   "name"
@@ -90,9 +90,9 @@ ActiveRecord::Schema.define(:version => 20131020235113) do
     t.integer  "assignable_id"
     t.string   "assignable_type"
     t.integer  "user_id"
-    t.string   "state"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.datetime "approved_at"
   end
 
   add_index "approval_assignments", ["assignable_type", "assignable_id"], :name => "index_approval_assignments_on_assignable_type_and_assignable_id"
