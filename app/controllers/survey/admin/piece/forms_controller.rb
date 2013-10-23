0a9e9f33727bdd57faaf7647c4ed834c39adfb85
zomeki <zomeki@zomeki.jp>
2013-10-23 13:27:25 +0900

    Stop using CKEditor on piece editing page

--- app/controllers/survey/admin/piece/forms_controller.rb
+++ app/controllers/survey/admin/piece/forms_controller.rb
@@ -1,12 +1,2 @@
 class Survey::Admin::Piece::FormsController < Cms::Admin::Piece::BaseController
-  def update
-    item_in_settings = (params[:item][:in_settings] || {})
-
-    item_in_settings[:target_form_id] = params[:target_form]
-    item_in_settings[:upper_text] = params[:upper_text]
-    item_in_settings[:lower_text] = params[:lower_text]
-
-    params[:item][:in_settings] = item_in_settings
-    super
-  end
 end
