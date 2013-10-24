b1ad4b3b2b87d4042eeaa874dbf0424537ba0f39
zomeki <zomeki@zomeki.jp>
2013-10-24 09:34:30 +0900

    Fix factory

--- spec/factories/survey_forms.rb
+++ spec/factories/survey_forms.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :survey_form_1, class: 'Survey::Form' do
     association :content, :factory => :survey_content_form_1
+    name 'form1'
     title '汎用アンケートのフォームその１'
   end
 end
