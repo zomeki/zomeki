65fb8b836b539c01a13c95292b01e6f6504b4b8b
Masaki Ozawa <ozamasa123@gmail.com>
2013-11-05 18:30:20 +0900

    Add create holidays data

--- lib/gp_calendar/controller/holiday.rb
+++ lib/gp_calendar/controller/holiday.rb
@@ -10,12 +10,27 @@ module GpCalendar::Controller::Holiday
           begin
             item = content.holidays.build(val)
             item.date ||= parse_date(val["date"]) rescue item.date = parse_date(val["date"], '%m-%d')
+            if item.date.blank?
+              begin
+                /(\d+)月の第(\d+)(\W+)曜日/ =~ val["date"]
+                item.date = specific_date($1, $2, $3)
+              rescue
+              end
+            end
             item.save
           rescue
           end
         end
       end
     end
+  end
+
+  def specific_date(month, times, wday, year = Time.now.year)
+    d = Date.new(year, month.to_i, 1)
+    i = ["日", "月", "火", "水", "木", "金", "土"].index(wday)
+    d = d + (d.wday > i ? (7 + i - d.wday).days : (i - d.wday).abs.days)
+    d = d + (7 * (times.to_i - 1)).days
+    return d
   end
 
   def parse_date(datestring, format='%m月%d日')
