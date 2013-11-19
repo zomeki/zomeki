0ec3a2e4a31c1d244ebe9f2707842082b87b9977
zomeki <zomeki@zomeki.jp>
2013-11-19 15:21:31 +0900

    Add passback and pullback to approval

--- app/models/approval/approval_request.rb
+++ app/models/approval/approval_request.rb
@@ -17,13 +17,20 @@ class Approval::ApprovalRequest < ActiveRecord::Base
     approval_flow.approvals.find_by_index(current_index)
   end
 
+  def min_index
+    0
+  end
+
   def max_index
     approval_flow.approvals.map(&:index).max
   end
 
   def approve(user)
-    current_assignments.find_by_user_id(user.id).try(:update_attribute, :approved_at, Time.now)
+    return false unless current_approvers.include?(user)
+
+    current_assignments.find_by_user_id(user.id).update_attribute(:approved_at, Time.now)
     current_assignments.reload # to flush cache
+
     if current_assignments.all?{|a| a.approved_at }
       if current_index == max_index
         yield('finish') if block_given?
@@ -37,6 +44,36 @@ class Approval::ApprovalRequest < ActiveRecord::Base
         yield('progress') if block_given?
       end
     end
+
+    return true
+  end
+
+  def passback(user, comment: nil)
+    return false unless current_approvers.include?(user)
+    comment ||= ''
+    transaction do
+      decrement!(:current_index) unless current_index == min_index
+      current_assignments.destroy_all
+      current_user_ids = current_approval.approver_ids
+      reload # to flush cache
+    end
+#TODO: コメントを書く
+p "差し戻し：#{comment}"
+    return true
+  end
+
+  def pullback(user, comment: nil)
+    return false unless self.user == user
+    comment ||= ''
+    transaction do
+      decrement!(:current_index) unless current_index == min_index
+      current_assignments.destroy_all
+      current_user_ids = current_approval.approver_ids
+      reload # to flush cache
+    end
+#TODO: コメントを書く
+p "引き戻し：#{comment}"
+    return true
   end
 
   def finished?
@@ -46,7 +83,7 @@ class Approval::ApprovalRequest < ActiveRecord::Base
   private
 
   def set_defaults
-    self.current_index ||= 0 if self.has_attribute?(:current_index)
+    self.current_index = min_index if self.has_attribute?(:current_index) && self.current_index.nil?
     self.current_approvers = current_approval.approvers if current_approvers.empty?
   end
 end
