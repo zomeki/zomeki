085f0e344b80db2a230da4b41c4020533f209aae
zomeki <zomeki@zomeki.jp>
2013-12-03 13:14:43 +0900

    Fix OG tags

--- app/helpers/gp_article/gp_article_helper.rb
+++ app/helpers/gp_article/gp_article_helper.rb
@@ -48,13 +48,13 @@ module GpArticle::GpArticleHelper
       case key
       when 'description'
         value = strip_tags(item.body) if item.og_description_use_body
-        tag :meta, name: 'og:description', content: value.to_s.gsub("\n", ' ')
+        tag :meta, property: 'og:description', content: value.to_s.gsub("\n", ' ')
       when 'image'
         if (file = item.image_files.detect{|f| f.name == value })
-          tag :meta, name: 'og:image', content: "#{item.content.public_node.public_full_uri}#{item.name}/file_contents/#{url_encode file.name}"
+          tag :meta, property: 'og:image', content: "#{item.content.public_node.public_full_uri}#{item.name}/file_contents/#{url_encode file.name}"
         end
       else
-        tag :meta, name: "og:#{key}", content: value
+        tag :meta, property: "og:#{key}", content: value
       end
     }.join.html_safe
   end
