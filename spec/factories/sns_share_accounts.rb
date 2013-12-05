d4d7a4b83f97464d0b9eeabe745c87a781b60d22
zomeki <zomeki@zomeki.jp>
2013-12-05 14:58:38 +0900

    Fix provider validation and spec

--- spec/factories/sns_share_accounts.rb
+++ spec/factories/sns_share_accounts.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :sns_share_account_1, :class => 'SnsShare::Account' do
-    association :content, :factory => :sns_share_content_account_1
+    content_id 1
     provider 'facebook'
+  end
+
+  factory :sns_share_account_2, :class => 'SnsShare::Account' do
+    content_id 1
+    provider 'twitter'
   end
 end
