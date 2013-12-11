88e2569a4c1be40f4757cdd496f78d6ed874743e
zomeki <zomeki@zomeki.jp>
2013-12-12 01:07:35 +0900

    Now Cms::Content.code set automatically

--- db/migrate/20131210142548_add_code_to_cms_contents.rb
+++ db/migrate/20131210142548_add_code_to_cms_contents.rb
@@ -1,5 +1,10 @@
 class AddCodeToCmsContents < ActiveRecord::Migration
-  def change
+  def up
     add_column :cms_contents, :code, :string
+    Cms::Content.all.each{|c| c.update_column(:code, "C#{c.id}") }
+  end
+
+  def down
+    remove_column :cms_contents, :code
   end
 end
