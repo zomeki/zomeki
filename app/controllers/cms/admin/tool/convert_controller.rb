3967b050256d79878c43bcc832770e85c7a0b399
MakeItEasy <rugang6891@gmail.com>
2013-12-18 11:14:31 +0900

    downloadとfile list機能の実装

--- app/controllers/cms/admin/tool/convert_controller.rb
+++ app/controllers/cms/admin/tool/convert_controller.rb
@@ -12,10 +12,45 @@ class Cms::Admin::Tool::ConvertController < Cms::Controller::Admin::Base
     def @item.site_url=(v) ; @site_url = v ; end
     if request.post?
       @item.site_url= params[:item][:site_url]
-      # TODO dairg download the site
-      Util::Convert.download_site(@item.site_url)
+      if @item.site_url.present?
+        Thread.fork(@item.site_url) do |site_url|
+          result = Util::Convert.download_site(site_url)
+          if result
+            puts "#{site_url} download successful completed!"
+          else
+            puts "#{site_url} download failure!"
+          end
+        end
+        sleep 1
+      end
       redirect_to tool_convert_url
     end
   end
+
+  # ファイル一覧
+  def file_list
+    @site_url  = params[:site_url] || ""
+    @root      = "#{Util::Convert::SITE_BASE_DIR}/#{@site_url}"
+    @path      = params[:path].to_s
+    @full_path = "#{@root}/#{@path}"
+    @base_uri  = ["#{Util::Convert::SITE_BASE_DIR}/", "/"]
+
+    @item = Tool::SiteContent.new(@site_url, @full_path, :root => @root, :base_uri => @base_uri)
+    return show    if params[:do] == 'show'
+
+    if params[:do].nil? && @item.file?
+      params[:do] = "show"
+      return show
+    end
+
+    @dirs  = @item.child_directories
+    @files = @item.child_files
+  end
+
+  def show
+    # @item.read_body
+    render :show
+  end
+
 end
 
--- lib/util/convert.rb
+++ lib/util/convert.rb
@@ -1,5 +1,18 @@
 # encoding: utf-8
 class Util::Convert
+  SITE_BASE_DIR = "#{Rails.application.root.to_s}/wget_sites"
+
   def self.download_site(url)
+    system "wget -rqN -P #{SITE_BASE_DIR} #{url}" if url
+  end
+
+  def self.all_site_urls
+    items = []
+    if ::FileTest.directory?(SITE_BASE_DIR)
+      Dir::entries(SITE_BASE_DIR).sort.each do |name|
+        items << name unless name =~ /^\.+/ || ::FileTest.file?(::File.join(SITE_BASE_DIR, name))
+      end
+    end
+    items
   end
 end
