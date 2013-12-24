943cd31941d7f642a713069d35cca5e03cafcbb1
zomeki <zomeki@zomeki.jp>
2013-12-24 16:36:25 +0900

    Setting comment now use i18n

--- app/models/gp_category/content/setting.rb
+++ app/models/gp_category/content/setting.rb
@@ -5,7 +5,7 @@ class GpCategory::Content::Setting < Cms::ContentSetting
   set_config :list_style, :name => "#{GpArticle::Doc.model_name.human}表示形式",
     :comment => I18n.t('comments.list_style').html_safe
   set_config :date_style, :name => "#{GpArticle::Doc.model_name.human}日付形式",
-    :comment => '年：%Y 月：%m 日：%d 時：%H 分：%M 秒：%S'
+    :comment => I18n.t('comments.date_style').html_safe
   set_config :category_type_style, :name => "#{GpCategory::CategoryType.model_name.human}表示形式",
     :options => GpCategory::Content::CategoryType::CATEGORY_TYPE_STYLE_OPTIONS
   set_config :category_style, :name => "#{GpCategory::Category.model_name.human}表示形式",
