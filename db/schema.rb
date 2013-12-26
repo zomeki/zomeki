3026cefa8b700d02d9bfcd29fe73d50b5c31c985
zomeki <zomeki@zomeki.jp>
2013-12-26 17:22:53 +0900

    Add body_more to GpArticle::Doc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131226020101) do
+ActiveRecord::Schema.define(:version => 20131226081329) do
 
   create_table "ad_banner_banners", :force => true do |t|
     t.string   "name"
@@ -721,6 +721,8 @@ ActiveRecord::Schema.define(:version => 20131226020101) do
     t.integer  "template_id"
     t.text     "template_values"
     t.string   "share_to_sns_with"
+    t.text     "body_more"
+    t.string   "body_more_link_text"
   end
 
   add_index "gp_article_docs", ["concept_id"], :name => "index_gp_article_docs_on_concept_id"
