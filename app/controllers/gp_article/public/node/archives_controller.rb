300fc5d6a17679b2349748dea481eae82a6548e6
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2014-01-14 09:07:05 +0900

    Complete archive list

--- app/controllers/gp_article/public/node/archives_controller.rb
+++ app/controllers/gp_article/public/node/archives_controller.rb
@@ -17,6 +17,20 @@ class GpArticle::Public::Node::ArchivesController < Cms::Controller::Public::Bas
     docs = GpArticle::Doc.arel_table
     @docs = @content.public_docs.where(docs[:display_published_at].gteq(started_at)
                                        .and(docs[:display_published_at].lteq(ended_at)))
+                                .order('display_published_at DESC, published_at DESC')
+
+    http_error(404) if @docs.empty?
+
+    @items = @docs.inject([]) do |result, doc|
+        date = doc.display_published_at.strftime('%Y年%-m月%-d日')
+
+        unless result.empty?
+          last_date = result.last[:doc].display_published_at.strftime('%Y年%-m月%-d日')
+          date = nil if date == last_date
+        end
+
+        result.push(date: date, doc: doc)
+      end
   end
 
   private
