35b2fc1b490ca19ac01ad1c33fefc354c452bd3b
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2014-01-14 11:00:48 +0900

    Apply blog functions setting

--- app/controllers/gp_article/public/node/comments_controller.rb
+++ app/controllers/gp_article/public/node/comments_controller.rb
@@ -32,7 +32,7 @@ class GpArticle::Public::Node::CommentsController < Cms::Controller::Public::Bas
 
     if simple_captcha_valid?
       if @comment.save
-        CommonMailer.commented_notification(@comment).deliver
+        CommonMailer.commented_notification(@comment).deliver if @content.blog_functions[:comment_notification_mail]
         redirect_to @doc.public_full_uri
       else
         render :new
