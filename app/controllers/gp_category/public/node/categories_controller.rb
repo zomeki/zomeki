66f64439849e5d8d44bf9695e18a3f94a42893e3
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2014-01-17 17:16:13 +0900

    Creating docs_2 template in progress...

--- app/controllers/gp_category/public/node/categories_controller.rb
+++ app/controllers/gp_category/public/node/categories_controller.rb
@@ -13,31 +13,41 @@ class GpCategory::Public::Node::CategoriesController < Cms::Controller::Public::
     @category = category_type.find_category_by_path_from_root_category(params[:category_names])
     return http_error(404) unless @category.try(:public?)
 
-    @docs = @category.public_docs
-
     if params[:format].in?('rss', 'atom')
-      @docs = @docs.display_published_after(@content.feed_docs_period.to_i.days.ago) if @content.feed_docs_period.present?
-      @docs = @docs.paginate(page: params[:page], per_page: @content.feed_docs_number)
-      return render_feed(@docs)
+      docs = @category.public_docs
+      docs = docs.display_published_after(@content.feed_docs_period.to_i.days.ago) if @content.feed_docs_period.present?
+      docs = docs.paginate(page: params[:page], per_page: @content.feed_docs_number)
+      return render_feed(docs)
     end
 
     Page.current_item = @category
     Page.title = @category.title
+
+    per_page = (@more ? 30 : @content.category_docs_number)
 
     if (template = category_type.template)
       rendered = template.body.gsub(/\[\[module\/([\w-]+)\]\]/) do |matched|
           tm = @content.template_modules.find_by_name($1)
           next unless tm
 
-          docs = @docs.paginate(page: params[:page], per_page: tm.num_docs)
+          category_ids = case tm.module_type
+                         when 'docs_1'
+                           [@category.id]
+                         when 'docs_2'
+                           @category.public_descendants.map(&:id)
+                         else
+                           []
+                         end
+
+          docs = find_public_docs_with_category_ids(category_ids).paginate(page: params[:page], per_page: per_page)
+#where tm.module_type_mode
+
           view_context.try(tm.module_type, category: @category, template_module: tm, docs: docs)
         end
 
       render text: rendered
     else
-      per_page = (@more ? 30 : @content.category_docs_number)
-
-      @docs = @docs.paginate(page: params[:page], per_page: per_page)
+      @docs = @category.public_docs.paginate(page: params[:page], per_page: per_page)
       return http_error(404) if @docs.current_page > @docs.total_pages
 
       if Page.mobile?
@@ -52,5 +62,13 @@ class GpCategory::Public::Node::CategoriesController < Cms::Controller::Public::
         end
       end
     end
+  end
+
+  private
+
+  def find_public_docs_with_category_ids(category_ids)
+    categorizations = GpCategory::Categorization.arel_table
+    GpArticle::Doc.mobile(::Page.mobile?).public
+                  .joins(:categorizations).where(categorizations[:category_id].in(category_ids))
   end
 end
