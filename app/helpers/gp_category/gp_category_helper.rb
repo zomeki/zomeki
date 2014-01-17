b9568371d308c05911dc0591fd69b995e8f168d4
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2014-01-17 15:43:52 +0900

    Add category layouts for template module

--- app/helpers/gp_category/gp_category_helper.rb
+++ app/helpers/gp_category/gp_category_helper.rb
@@ -1,4 +1,19 @@
 module GpCategory::GpCategoryHelper
+  def category_li(category, depth_limit: 100, depth: 1)
+    content_tag(:li) do
+      result = link_to(category.title, category.public_uri)
+      if category.public_children.empty? || depth >= depth_limit
+        result
+      else
+        result << content_tag(:ul) do
+            category.public_children.inject(''){|lis, child|
+              lis << category_li(child, depth_limit: depth_limit, depth: depth + 1)
+            }.html_safe
+          end
+      end
+    end
+  end
+
   def categories_1(category:, template_module:, docs:)
     return if category.public_children.empty?
 
@@ -9,18 +24,23 @@ module GpCategory::GpCategoryHelper
     end
   end
 
-  def category_li(category)
-    content_tag(:li) do
-      result = link_to(category.title, category.public_uri)
-      if category.public_children.empty?
-        result
-      else
-        result << content_tag(:ul) do
-            category.public_children.inject(''){|lis, child|
-              lis << category_li(child)
-            }.html_safe
-          end
-      end
+  def categories_2(category:, template_module:, docs:)
+    return if category.public_children.empty?
+
+    content_tag(:ul) do
+      category.public_children.inject(''){|lis, child|
+        lis << category_li(child, depth_limit: 1)
+      }.html_safe
+    end
+  end
+
+  def categories_3(category:, template_module:, docs:)
+    return if category.public_children.empty?
+
+    content_tag(:ul) do
+      category.public_children.inject(''){|lis, child|
+        lis << category_li(child, depth_limit: 2)
+      }.html_safe
     end
   end
 end
