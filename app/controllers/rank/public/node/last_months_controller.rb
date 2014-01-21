c3414ea752efa5fe99379d51af8db4c55390976c
Masaki Ozawa <ozamasa123@gmail.com>
2014-01-21 23:51:47 +0900

    Add condition of hostname

--- app/controllers/rank/public/node/last_months_controller.rb
+++ app/controllers/rank/public/node/last_months_controller.rb
@@ -15,9 +15,17 @@ class Rank::Public::Node::LastMonthsController < Cms::Controller::Public::Base
     select_col = 'pageviews' # 'visitors'
     per_page   = 20
     exclusion  = @content.setting_value(:exclusion_url).strip.split(/[ |\t|\r|\n|\f]+/) rescue exclusion = ''
+    hostname   = URI.parse(Core.site.full_uri).host
 
     rank_table = Rank::Rank.arel_table
-    @ranks = @content.ranks.where(rank_table[:date].gteq(from.strftime('%F')).and(rank_table[:date].lteq(to.strftime('%F')))).where(rank_table[:page_path].not_in(exclusion)).select('*').select(rank_table[select_col].sum.as('accesses')).group(rank_table[:page_path]).order('accesses DESC').paginate(page: params[:page], per_page: per_page)
+    @ranks = @content.ranks.where(rank_table[:date].gteq(from.strftime('%F')).and(rank_table[:date].lteq(to.strftime('%F'))))
+                           .where(rank_table[:hostname].eq(hostname))
+                           .where(rank_table[:page_path].not_in(exclusion))
+                           .select('*')
+                           .select(rank_table[select_col].sum.as('accesses'))
+                           .group(rank_table[:page_path])
+                           .order('accesses DESC')
+                           .paginate(page: params[:page], per_page: per_page)
 
   end
 end
