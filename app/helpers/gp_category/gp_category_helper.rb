02e9e1c9d39af9a74e50eb6ec3e0e4634bc769e6
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2014-01-27 16:30:12 +0900

    Change helper method arguments

--- app/helpers/gp_category/gp_category_helper.rb
+++ app/helpers/gp_category/gp_category_helper.rb
@@ -18,31 +18,31 @@ module GpCategory::GpCategoryHelper
     end
   end
 
-  def categories_1(template_module: nil, category: nil)
-    return if category.public_children.empty?
+  def categories_1(template_module: nil, categories: nil)
+    return if categories.empty?
 
     content_tag(:ul) do
-      category.public_children.inject(''){|lis, child|
+      categories.inject(''){|lis, child|
         lis << category_li(child)
       }.html_safe
     end
   end
 
-  def categories_2(template_module: nil, category: nil)
-    return if category.public_children.empty?
+  def categories_2(template_module: nil, categories: nil)
+    return if categories.empty?
 
     content_tag(:ul) do
-      category.public_children.inject(''){|lis, child|
+      categories.inject(''){|lis, child|
         lis << category_li(child, depth_limit: 1)
       }.html_safe
     end
   end
 
-  def categories_3(template_module: nil, category: nil)
-    return if category.public_children.empty?
+  def categories_3(template_module: nil, categories: nil)
+    return if categories.public_children.empty?
 
     content_tag(:ul) do
-      category.public_children.inject(''){|lis, child|
+      categories.inject(''){|lis, child|
         lis << category_li(child, depth_limit: 2)
       }.html_safe
     end
