b5956ec152c842693b972d4aeefaa0e6226ce0e1
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2014-01-31 16:45:31 +0900

    Redirect after file uploading

--- app/controllers/gp_article/admin/docs/files_controller.rb
+++ app/controllers/gp_article/admin/docs/files_controller.rb
@@ -63,7 +63,7 @@ class GpArticle::Admin::Docs::FilesController < Cms::Controller::Admin::Base
     flash.now[:alert]  = "#{failure}件の登録処理に失敗しました。" if failure != 0
 
     @items = Sys::File.where(tmp_id: @tmp_unid, parent_unid: @doc.try(:unid)).paginate(page: params[:page], per_page: 20).order(:name)
-    render :index
+    redirect_to url_for(:action => :index)
   end
 
   def update
