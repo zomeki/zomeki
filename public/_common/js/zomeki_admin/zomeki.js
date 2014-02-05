36a9aa3ffd8ef580d0b5397988904692204f04e7
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2014-02-05 18:23:20 +0900

    Switch CKEditor toolbar by client

---
+++ public/_common/js/zomeki_admin/zomeki.js
@@ -0,0 +1,12 @@
+(function (window, undefined) {
+  if (window.zomeki !== undefined) {
+    return;
+  }
+
+  var zomeki = {
+    Core: {},
+    Page: {}
+  };
+
+  window.zomeki = zomeki;
+})(window);
