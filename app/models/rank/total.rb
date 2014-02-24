dab512b5f5cb4b3a2c35cd7bc957e1ee0645c424
Masaki Ozawa <ozamasa123@gmail.com>
2014-02-24 15:34:21 +0900

    Remove site name from ranking title

--- app/models/rank/total.rb
+++ app/models/rank/total.rb
@@ -5,4 +5,8 @@ class Rank::Total < ActiveRecord::Base
   belongs_to :content, foreign_key: :content_id, class_name: 'Rank::Content::Rank'
   validates_presence_of :content_id
 
+  def page_title
+    self[:page_title].gsub(' | ' + Core.site.name, '')
+  end
+
 end
