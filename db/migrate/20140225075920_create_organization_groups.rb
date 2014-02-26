ab7a2dc53b8c7163610970b924cba83bed160b78
zomeki <zomeki@zomeki.jp>
2014-02-26 12:34:09 +0900

    Add Organization::Group with admin pages

---
+++ db/migrate/20140225075920_create_organization_groups.rb
@@ -0,0 +1,16 @@
+class CreateOrganizationGroups < ActiveRecord::Migration
+  def change
+    create_table :organization_groups do |t|
+      t.belongs_to :content
+      t.string :state
+      t.string :name
+      t.string :sys_group_code
+      t.string :sitemap_state
+      t.string :docs_order
+      t.integer :sort_no
+
+      t.timestamps
+    end
+    add_index :organization_groups, :sys_group_code
+  end
+end
