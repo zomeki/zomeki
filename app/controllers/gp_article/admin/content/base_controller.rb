c6a7e390f0ca87ac798fa39e48d7eb051f88b842
zomeki <zomeki@zomeki.jp>
2014-02-26 23:31:58 +0900

    Specify the class for content operation

--- app/controllers/gp_article/admin/content/base_controller.rb
+++ app/controllers/gp_article/admin/content/base_controller.rb
@@ -1,3 +1,6 @@
 # encoding: utf-8
 class GpArticle::Admin::Content::BaseController < Cms::Admin::Content::BaseController
+  def model
+    GpArticle::Content::Doc
+  end
 end
--- app/controllers/gp_category/admin/content/base_controller.rb
+++ app/controllers/gp_category/admin/content/base_controller.rb
@@ -1,3 +1,6 @@
 # encoding: utf-8
 class GpCategory::Admin::Content::BaseController < Cms::Admin::Content::BaseController
+  def model
+    GpCategory::Content::CategoryType
+  end
 end
