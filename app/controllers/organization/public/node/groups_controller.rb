d43c3b17c977fb34c9df22415ec51352da70d92d
zomeki <zomeki@zomeki.jp>
2014-02-28 00:57:16 +0900

    Add more page

--- app/controllers/organization/public/node/groups_controller.rb
+++ app/controllers/organization/public/node/groups_controller.rb
@@ -2,6 +2,7 @@ class Organization::Public::Node::GroupsController < Cms::Controller::Public::Ba
   def pre_dispatch
     @content = Organization::Content::Group.find_by_id(Page.current_node.content.id)
     return http_error(404) unless @content
+    @more = (params[:filename_base] =~ /^more($|_)/i)
   end
 
   def index
@@ -17,9 +18,13 @@ class Organization::Public::Node::GroupsController < Cms::Controller::Public::Ba
     Page.current_item = @group
     Page.title = @group.sys_group.name
 
+    per_page = (@more ? 30 : @content.num_docs)
+
     sys_group_ids = @group.public_descendants.map{|g| g.sys_group.id }
     @docs = find_public_docs_with_group_id(sys_group_ids).order(@group.docs_order)
-                                 .paginate(page: params[:page], per_page: @content.num_docs)
+                                                         .paginate(page: params[:page], per_page: per_page)
+
+    render 'more' if @more
   end
 
   private
