3bb6042a1c772457349c2c4094b6fdd615363eae
zomeki <zomeki@zomeki.jp>
2014-02-28 13:47:47 +0900

    Specify each of content classes

--- app/controllers/sns_share/admin/content/base_controller.rb
+++ app/controllers/sns_share/admin/content/base_controller.rb
@@ -1,2 +1,5 @@
 class SnsShare::Admin::Content::BaseController < Cms::Admin::Content::BaseController
+  def model
+    SnsShare::Content::Account
+  end
 end
--- app/controllers/tag/admin/content/base_controller.rb
+++ app/controllers/tag/admin/content/base_controller.rb
@@ -1,3 +1,6 @@
 # encoding: utf-8
 class Tag::Admin::Content::BaseController < Cms::Admin::Content::BaseController
+  def model
+    Tag::Content::Tag
+  end
 end
