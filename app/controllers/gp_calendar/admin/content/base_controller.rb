1b4e7703b592403fbe1658707573f766349ff7a2
zomeki <zomeki@zomeki.jp>
2014-02-28 13:47:47 +0900

    Specify each of content classes

--- app/controllers/gp_calendar/admin/content/base_controller.rb
+++ app/controllers/gp_calendar/admin/content/base_controller.rb
@@ -1,3 +1,6 @@
 # encoding: utf-8
 class GpCalendar::Admin::Content::BaseController < Cms::Admin::Content::BaseController
+  def model
+    GpCalendar::Content::Event
+  end
 end
--- app/controllers/map/admin/content/base_controller.rb
+++ app/controllers/map/admin/content/base_controller.rb
@@ -1,3 +1,6 @@
 # encoding: utf-8
 class Map::Admin::Content::BaseController < Cms::Admin::Content::BaseController
+  def model
+    Map::Content::Marker
+  end
 end
