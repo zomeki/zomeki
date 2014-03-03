9a6f027e3f8b4a850eceeaa15d69a4cd46a103ce
zomeki <zomeki@zomeki.jp>
2014-03-03 12:50:17 +0900

    Set default filename_base

--- db/migrate/20140221050927_add_filename_base_to_gp_article_docs.rb
+++ db/migrate/20140221050927_add_filename_base_to_gp_article_docs.rb
@@ -1,5 +1,10 @@
 class AddFilenameBaseToGpArticleDocs < ActiveRecord::Migration
-  def change
+  def up
     add_column :gp_article_docs, :filename_base, :string
+    GpArticle::Doc.where(filename_base: nil).update_all(filename_base: 'index')
+  end
+
+  def down
+    remove_column :gp_article_docs, :filename_base
   end
 end
