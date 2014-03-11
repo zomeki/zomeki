982a29d0319459ecad11f739c591c135bea43e19
zomeki <zomeki@zomeki.jp>
2014-03-11 11:58:52 +0900

    Write messages to log

--- app/controllers/cms/script/nodes_controller.rb
+++ app/controllers/cms/script/nodes_controller.rb
@@ -33,7 +33,7 @@ class Cms::Script::NodesController < Cms::Controller::Script::Publication
           uri = "#{child_node.public_uri}?node_id=#{child_node.id}"
           publish_page(child_node, :uri => uri, :site => child_node.site, :path => child_node.public_path)
         rescue => e
-          puts "Error: #{e}"
+          error_log e.message
         end
         next
       end
@@ -47,10 +47,10 @@ class Cms::Script::NodesController < Cms::Controller::Script::Publication
           publish_page(child_node, :uri => child_node.public_uri, :site => child_node.site, :path => child_node.public_path)
           res = render_component_into_view :controller => model, :action => 'publish', :params => params.merge(node: child_node)
         rescue LoadError => e
-          puts "Error: #{e}"
+          error_log e.message
           next
         rescue Exception => e
-          puts "Error: #{e}"
+          error_log e.message
           next
         end
       end
@@ -60,49 +60,47 @@ class Cms::Script::NodesController < Cms::Controller::Script::Publication
   end
 
   def publish_by_task
-    begin
-      item = params[:item]
-      if item.state == 'recognized' && item.model == 'Cms::Page'
-        puts "-- Publish: #{item.class}##{item.id}"
-        item = Cms::Node::Page.find(item.id)
-        uri  = "#{item.public_uri}?node_id=#{item.id}"
-        path = "#{item.public_path}"
+    item = params[:item]
+    if item.state == 'recognized' && item.model == 'Cms::Page'
+      info_log "-- Publish: #{item.class}##{item.id}"
+      item = Cms::Node::Page.find(item.id)
+      uri  = "#{item.public_uri}?node_id=#{item.id}"
+      path = "#{item.public_path}"
 
-        unless item.publish(render_public_as_string(uri, site: item.site))
-          raise item.errors.full_messages
-        end
-
-        ruby_uri  = (uri =~ /\?/) ? uri.gsub(/(.*\.html)\?/, '\\1.r?') : "#{uri}.r"
-        ruby_path = "#{path}.r"
-        if item.published? || !::File.exist?(ruby_uri)
-          item.publish_page(render_public_as_string(ruby_uri, :site => item.site),
-            :path => ruby_path, :dependent => :ruby)
-        end
-
-        puts 'OK: Published'
-        params[:task].destroy
+      unless item.publish(render_public_as_string(uri, site: item.site))
+        raise item.errors.full_messages
       end
-    rescue => e
-      puts "Error: #{e}"
+
+      ruby_uri  = (uri =~ /\?/) ? uri.gsub(/(.*\.html)\?/, '\\1.r?') : "#{uri}.r"
+      ruby_path = "#{path}.r"
+      if item.published? || !::File.exist?(ruby_uri)
+        item.publish_page(render_public_as_string(ruby_uri, :site => item.site),
+          :path => ruby_path, :dependent => :ruby)
+      end
+
+      info_log %Q!OK: Published to "#{path}"!
+      params[:task].destroy
     end
     render text: 'OK'
+  rescue => e
+    error_log e.message
+    render text: 'NG'
   end
 
   def close_by_task
-    begin
-      item = params[:item]
-      if item.state == 'public' && item.model == 'Cms::Page'
-        puts "-- Close: #{item.class}##{item.id}"
-        item = Cms::Node::Page.find(item.id)
+    item = params[:item]
+    if item.state == 'public' && item.model == 'Cms::Page'
+      info_log "-- Close: #{item.class}##{item.id}"
+      item = Cms::Node::Page.find(item.id)
 
-        item.close
+      item.close
 
-        puts 'OK: Closed'
-        params[:task].destroy
-      end
-    rescue => e
-      puts "Error: #{e}"
+      info_log 'OK: Closed'
+      params[:task].destroy
     end
     render text: 'OK'
+  rescue => e
+    error_log e.message
+    render text: 'NG'
   end
 end
