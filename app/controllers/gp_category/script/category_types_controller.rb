dc25d9ea7cf4485d693bfa501c882ef857d435c3
zomeki <zomeki@zomeki.jp>
2014-03-11 18:40:08 +0900

    Script runner now accepts target child

--- app/controllers/gp_category/script/category_types_controller.rb
+++ app/controllers/gp_category/script/category_types_controller.rb
@@ -5,14 +5,27 @@ class GpCategory::Script::CategoryTypesController < Cms::Controller::Script::Pub
     path = "#{@node.public_path}"
     publish_more(@node, :uri => uri, :path => path, :first => 2, :dependent => :more)
 
-    @node.content.public_category_types.each do |category_type|
+    category_types = if (id = params[:target_id]).present?
+                       @node.content.public_category_types.where(id: id)
+                     else
+                       @node.content.public_category_types
+                     end
+
+    category_types.each do |category_type|
       uri  = "#{@node.public_uri}#{category_type.name}/"
       path = "#{@node.public_path}#{category_type.name}/"
       publish_page(category_type, :uri => "#{uri}index.rss", :path => "#{path}index.rss", :dependent => "#{category_type.name}/rss")
       publish_page(category_type, :uri => "#{uri}index.atom", :path => "#{path}index.atom", :dependent => "#{category_type.name}/atom")
       publish_more(category_type, :uri => uri, :path => path, :dependent =>"#{category_type.name}/more")
-      category_type.public_root_categories.each do |category|
-        publish_category(category)
+
+      if (child_id = params[:target_child_id]).present?
+        category_type.public_categories.where(id: child_id).each do |category|
+          publish_category(category, follow_children: false)
+        end
+      else
+        category_type.public_root_categories.each do |category|
+          publish_category(category)
+        end
       end
     end
 
@@ -29,7 +42,7 @@ class GpCategory::Script::CategoryTypesController < Cms::Controller::Script::Pub
     GpCategory::Piece::Feed.where(:id => feed_piece_ids).all
   end
 
-  def publish_category(cat)
+  def publish_category(cat, follow_children: true)
     cat_path = "#{cat.category_type.name}/#{cat.path_from_root_category}/"
     uri = "#{@node.public_uri}#{cat_path}"
     path = "#{@node.public_path}#{cat_path}"
@@ -49,6 +62,8 @@ class GpCategory::Script::CategoryTypesController < Cms::Controller::Script::Pub
     end
 
     info_log %Q!OK: Published to "#{path}"!
+
+    return unless follow_children
 
     cat.public_children.each do |c|
       publish_category(c)
