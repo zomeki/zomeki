9a03072229c6b0ba6e6f129aef75a62e11e47b72
zomeki <zomeki@zomeki.jp>
2014-03-13 15:34:26 +0900

    Clean duplicated jobs

---
+++ app/models/gp_category/publisher.rb
@@ -0,0 +1,41 @@
+class GpCategory::Publisher < ActiveRecord::Base
+  include Sys::Model::Base
+
+  attr_accessible :category_id
+
+  belongs_to :category
+
+  validates :category_id, :presence => true, :uniqueness => true
+
+  def self.register_category(category)
+    self.where(category_id: category.id).first_or_create
+  end
+
+  def self.register_categories(categories)
+    categories.each{|c| self.register_category(c) }
+  end
+
+  def self.publish_categories
+    category_ids = {}
+    self.all.each do |publisher|
+      next unless publisher.category
+      c = publisher.category
+      category_ids[c.content.id] = {} unless category_ids[c.content.id]
+      category_ids[c.content.id][c.category_type.id] = [] unless category_ids[c.content.id][c.category_type.id]
+      category_ids[c.content.id][c.category_type.id] << c.id
+      publisher.destroy
+    end
+
+    script_params = []
+    category_ids.each do |key, value|
+      value.each do |k, v|
+        script_params <<
+          "target_module=gp_category&target_content_id[]=#{key}&target_id[]=#{k}&#{v.map{|c| "target_child_id[]=#{c}" }.join('&') }"
+      end
+    end
+
+    script_params.each do |script_param|
+      ::Script.run("cms/script/nodes/publish?#{script_param}", force: true)
+    end
+  end
+end
