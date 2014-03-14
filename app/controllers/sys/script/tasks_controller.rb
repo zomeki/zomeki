9c3773bbd3540043a560963e54348c5a62c440cf
zomeki <zomeki@zomeki.jp>
2014-03-14 11:33:20 +0900

    Add includes according to a bullet log

--- app/controllers/sys/script/tasks_controller.rb
+++ app/controllers/sys/script/tasks_controller.rb
@@ -1,13 +1,11 @@
 class Sys::Script::TasksController < ApplicationController
   def exec
-    task = Sys::Task.new
-    task.and :process_at, '<=', Time.now + (60 * 5) # before 5 min
-    tasks = task.find(:all, :order => :process_at)
-    
-    if tasks.size == 0
-      return render(:text => "No Tasks")
-    end
-    
+    tasks = Sys::Task.where(Sys::Task.arel_table[:process_at].lteq(5.minutes.ago))
+                     .order(:process_at)
+                     .includes(:unid_data)
+
+    return render(:text => 'No Tasks') if tasks.empty?
+
     tasks.each do |task|
       begin
         unless unid = task.unid_data
