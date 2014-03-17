1a9845447c9de86a70e962d5a524d71d554d4564
Masaki Ozawa <ozamasa123@gmail.com>
2014-03-17 15:39:56 +0900

    Change basic_auth_users for multi site

--- app/controllers/cms/admin/site/basic_auth_users_controller.rb
+++ app/controllers/cms/admin/site/basic_auth_users_controller.rb
@@ -1,24 +1,21 @@
 # encoding: utf-8
 class Cms::Admin::Site::BasicAuthUsersController < Cms::Controller::Admin::Base
   include Sys::Controller::Scaffold::Base
-  
+
   def pre_dispatch
     return error_auth unless Core.user.has_auth?(:manager)
     @site = Cms::Site.find(params[:site])
   end
-  
+
   def index
-    item = Cms::SiteBasicAuthUser.new
-    item.page  params[:page], params[:limit]
-    item.order params[:sort], :id
-    @items = item.find(:all)
+    @items = Cms::SiteBasicAuthUser.where(site_id: Core.site.id).paginate(page: params[:page], per_page: params[:limit])
     _index @items
   end
-  
+
   def show
     @item = Cms::SiteBasicAuthUser.new.find(params[:id])
     return error_auth unless @item.readable?
-    
+
     _show @item
   end
 
@@ -28,37 +25,37 @@ class Cms::Admin::Site::BasicAuthUsersController < Cms::Controller::Admin::Base
       :state      => 'enabled',
     })
   end
-  
+
   def create
     @item = Cms::SiteBasicAuthUser.new(params[:item])
     @item.site_id = @site.id
     _create @item do
     end
   end
-  
+
   def update
     @item = Cms::SiteBasicAuthUser.new.find(params[:id])
     @item.attributes = params[:item]
     _update @item do
     end
   end
-  
+
   def destroy
     @item = Cms::SiteBasicAuthUser.new.find(params[:id])
     _destroy(@item) do
     end
   end
-  
+
   def enable_auth
     @site.enable_basic_auth
-    
+
     flash[:notice] = "Basic認証を有効にしました。"
     redirect_to cms_site_basic_auth_users_path(@site.id)
   end
-  
+
   def disable_auth
     @site.disable_basic_auth
-    
+
     flash[:notice] = "Basic認証を無効にしました。"
     redirect_to cms_site_basic_auth_users_path(@site.id)
   end
