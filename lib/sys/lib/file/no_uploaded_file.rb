b7a18e9d2ad9c26b17a2c45cc3c4a87fdd61bb68
zomeki <zomeki@zomeki.jp>
2014-03-17 20:05:21 +0900

    Check file format with "file command" before RMagick for response quickness

--- lib/sys/lib/file/no_uploaded_file.rb
+++ lib/sys/lib/file/no_uploaded_file.rb
@@ -1,70 +1,77 @@
+require 'mime/types'
+require 'RMagick'
+
 class Sys::Lib::File::NoUploadedFile
   def initialize(path, options = {})
-    if path.class == Hash
-      options    = path
-      @data      = options[:data]
-    else
-      require 'mime/types'
-      file       = ::File.new(path)
-      @data      = file.read
+    not_image = false
+
+    case path
+    when Hash
+      options = path
+      @data = options[:data]
+    when String
+      @data = ::File.read(path)
       @mime_type = MIME::Types.type_for(path)[0].to_s
+
+      `type file > /dev/null 2>&1`
+      if $?.exitstatus == 0
+        not_image = `file #{path}` !~ /GIF|JPEG|PNG/
+      end
     end
-    @filename  = options[:filename]
-    @size      = @data.size if @data
-    @image     = validate_image
+
+    @filename = options[:filename]
+    @size = @data.size if @data
+
+    @image = validate_image unless not_image
   end
-  
+
   def errors
     @errors
   end
-  
+
   def read
     @data
   end
-  
+
   def original_filename
     @original_filename
   end
-  
+
   def size
     @size
   end
-  
+
   def mime_type
     @image ? @image.mime_type : @mime_type
   end
-  
+
   def content_type
     mime_type
   end
-  
+
   def image_is
     @image ? 1 : 2
   end
-  
+
   def image_width
     @image ? @image.columns : nil
   end
-  
+
   def image_height
     @image ? @image.rows : nil
   end
-  
+
   def validate_image
     begin
-      require 'RMagick'
-      image = Magick::Image.from_blob(@data).shift
-      if image.format =~ /(GIF|JPEG|PNG)/
-        return image
+      image = Magick::Image.from_blob(@data).first
+      if image.format =~ /GIF|JPEG|PNG/
+        image
+      else
+        nil
       end
-    rescue LoadError
-      return nil
-    rescue Magick::ImageMagickError
-      return nil
-    rescue NoMethodError
-      return nil
-    rescue
-      return nil
+    rescue => e
+      warn_log "#{e.class}: #{e.message}"
+      nil
     end
   end
 end
