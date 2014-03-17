3b9fa2e041b421a7b99be7bbdfe92f4acc06ba03
Masaki Ozawa <ozamasa123@gmail.com>
2014-03-17 15:10:56 +0900

    Change emergency for multi site

--- app/models/cms/site_setting/emergency_layout.rb
+++ app/models/cms/site_setting/emergency_layout.rb
@@ -1,12 +1,11 @@
 # encoding: utf-8
 class Cms::SiteSetting::EmergencyLayout < Cms::SiteSetting
   belongs_to :layout, :foreign_key => :value, :class_name => 'Cms::Layout'
-  
+
   validates_presence_of :value
   validates_uniqueness_of :value, :scope => :name
-  
+
   def current_site
-    self.and :site_id, Core.site.id
-    self
+    Cms::SiteSetting::EmergencyLayout.where(site_id: Core.site.id)
   end
 end
