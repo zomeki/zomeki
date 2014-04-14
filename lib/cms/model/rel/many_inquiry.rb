0e83608502e162339e7841c2a9f265eba9c4b0f6
Masaki Ozawa <ozamasa123@gmail.com>
2014-04-14 18:19:48 +0900

    Change inquery

--- lib/cms/model/rel/many_inquiry.rb
+++ lib/cms/model/rel/many_inquiry.rb
@@ -1,18 +1,18 @@
 # encoding: utf-8
 module Cms::Model::Rel::ManyInquiry
-  
+
   def self.included(mod)
     mod.has_many :inquiries, :foreign_key => 'parent_unid', :primary_key => 'unid', :class_name => 'Cms::Inquiry',
       :dependent => :destroy
     mod.accepts_nested_attributes_for :inquiries, :allow_destroy => true
-    
+
     mod.after_save :save_inquiry_parent_unid
   end
-  
+
   def inquiry_states
     [['表示','visible'],['非表示','hidden']]
   end
-  
+
   def build_default_inquiry(params = {})
     if inquiries.size == 0
       if g =  Core.user.group
@@ -22,18 +22,21 @@ module Cms::Model::Rel::ManyInquiry
       end
     end
   end
-  
+
   def validate_inquiry
     return true if content && !content.inquiry_related?
-    
+
     inquiries.each_with_index do |inquiry, i|
       next unless inquiry.visible?
-      
+
       if inquiry_display_require_field?(:group_id) && inquiry.group_id.blank?
         inquiry.errors.add(:group_id, :blank)
       end
       if inquiry_display_require_field?(:charge) && inquiry.charge.blank?
         inquiry.errors.add(:charge, :blank)
+      end
+      if inquiry_display_require_field?(:address) && inquiry.address.blank?
+        inquiry.errors.add(:address, :blank)
       end
       if inquiry_display_require_field?(:tel) && inquiry.tel.blank?
         inquiry.errors.add(:tel, :blank)
@@ -44,25 +47,29 @@ module Cms::Model::Rel::ManyInquiry
       if inquiry_display_require_field?(:email) && inquiry.email.blank?
         inquiry.errors.add(:email, :blank)
       end
-      
+      if inquiry_display_require_field?(:note) && inquiry.note.blank?
+        inquiry.errors.add(:note, :blank)
+      end
+
       inquiry.errors.add(:tel, :onebyte_characters) if inquiry.tel.to_s !~/^[ -~｡-ﾟ]*$/
       inquiry.errors.add(:fax, :onebyte_characters) if inquiry.fax.to_s !~/^[ -~｡-ﾟ]*$/
       inquiry.errors.add(:email, :invalid) if inquiry.email.to_s !~/^[ -~｡-ﾟ]*$/
     end
-    
+
     inquiries.each do |inquiry|
       inquiry.errors.each do |key|
         errors.add(:"inquiries/#{key}", inquiry.errors[key]) unless errors.include?(:"inquiries/#{key}")
       end
     end
   end
-  
+
   def save_inquiry_parent_unid
     inquiries.each do |inquiry|
       inquiry.update_attribute(:parent_unid, unid) if inquiry.parent_unid.blank?
+      inquiry.destroy if inquiry.group_id.blank?
     end
   end
-  
+
   def default_inquiry_state
     if content && content.inquiry_extra_values
       content.inquiry_extra_values[:state]
@@ -70,15 +77,15 @@ module Cms::Model::Rel::ManyInquiry
       'hidden'
     end
   end
-  
+
   def inquiry_display_fields
     if content && content.inquiry_extra_values
       content.inquiry_extra_values[:display_fields]
     else
-      ['group_id', 'charge', 'tel', 'fax', 'email']
+      ['group_id', 'charge', 'address', 'tel', 'fax', 'email', 'note']
     end
   end
-  
+
   def inquiry_require_fields
     if content && content.inquiry_extra_values
       content.inquiry_extra_values[:require_fields] || []
@@ -86,15 +93,15 @@ module Cms::Model::Rel::ManyInquiry
       []
     end
   end
-  
+
   def inquiry_display_field?(name)
     inquiry_display_fields.include?(name.to_s)
   end
-  
+
   def inquiry_require_field?(name)
     inquiry_require_fields.include?(name.to_s)
   end
-  
+
   def inquiry_display_require_field?(name)
     inquiry_display_field?(name) && inquiry_require_field?(name)
   end
