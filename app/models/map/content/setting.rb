c468d32cc6021d274ee2a0e45ab08f60ee271688
Masaki Ozawa <ozamasa123@gmail.com>
2014-05-27 16:39:02 +0900

    change category_type setting for multi-sites

--- app/models/map/content/setting.rb
+++ app/models/map/content/setting.rb
@@ -1,16 +1,16 @@
 # encoding: utf-8
 class Map::Content::Setting < Cms::ContentSetting
-  set_config :gp_category_content_category_type_id, :name => '汎用カテゴリタイプ',
-    :options => lambda { GpCategory::Content::CategoryType.all.map {|ct| [ct.name, ct.id] } }
-  set_config :lat_lng, :name => '地図/デフォルト座標',
-    :comment => '（緯度,経度）'
-  set_config :show_images, :name => '画像表示',
-    :options => Map::Content::Marker::IMAGE_STATE_OPTIONS,
-    :form_type => :radio_buttons
-  set_config :default_image, :name => '初期画像',
-    :comment => '（例 /images/sample.jpg ）'
-  set_config :marker_order, :name => '並び順',
-    :options => Map::Content::Marker::MARKER_ORDER_OPTIONS
+  set_config :gp_category_content_category_type_id, name: '汎用カテゴリタイプ',
+    options: lambda { GpCategory::Content::CategoryType.where(site_id: Core.site.id).map {|ct| [ct.name, ct.id] } }
+  set_config :lat_lng, name: '地図/デフォルト座標',
+    comment: '（緯度,経度）'
+  set_config :show_images, name: '画像表示',
+    options: Map::Content::Marker::IMAGE_STATE_OPTIONS,
+    form_type: :radio_buttons
+  set_config :default_image, name: '初期画像',
+    comment: '（例 /images/sample.jpg ）'
+  set_config :marker_order, name: '並び順',
+    options: Map::Content::Marker::MARKER_ORDER_OPTIONS
 
   def upper_text
   end
