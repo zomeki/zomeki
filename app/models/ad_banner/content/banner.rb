182946dfe6f03ce27b3efcec6b4f1fadc53f075f
zomeki <zomeki@zomeki.jp>
2014-05-31 01:09:34 +0900

    Add banner publication

--- app/models/ad_banner/content/banner.rb
+++ app/models/ad_banner/content/banner.rb
@@ -5,6 +5,11 @@ class AdBanner::Content::Banner < Cms::Content
   has_many :banners, :foreign_key => :content_id, :class_name => 'AdBanner::Banner', :dependent => :destroy
   has_many :groups, :foreign_key => :content_id, :class_name => 'AdBanner::Group', :dependent => :destroy
 
+  def public_node
+    Cms::Node.where(state: 'public', content_id: id, model: 'AdBanner::Banner').order(:id).first
+  end
+
+#TODO: DEPRECATED
   def banner_node
     return @banner_node if @banner_node
     @banner_node = Cms::Node.where(state: 'public', content_id: id, model: 'AdBanner::Banner').order(:id).first
