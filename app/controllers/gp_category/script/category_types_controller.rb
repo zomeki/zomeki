4d75725931d28bc1c3cdfde508306617f6bbcb51
zomeki <zomeki@zomeki.jp>
2014-06-09 22:34:45 +0900

    Rebuild categories for smartphone

--- app/controllers/gp_category/script/category_types_controller.rb
+++ app/controllers/gp_category/script/category_types_controller.rb
@@ -46,11 +46,14 @@ class GpCategory::Script::CategoryTypesController < Cms::Controller::Script::Pub
     cat_path = "#{cat.category_type.name}/#{cat.path_from_root_category}/"
     uri = "#{@node.public_uri}#{cat_path}"
     path = "#{@node.public_path}#{cat_path}"
+    smart_phone_path = "#{@node.public_smart_phone_path}#{cat_path}"
 
     publish_page(cat.category_type, :uri => "#{uri}index.rss", :path => "#{path}index.rss", :dependent => "#{cat_path}rss")
     publish_page(cat.category_type, :uri => "#{uri}index.atom", :path => "#{path}index.atom", :dependent => "#{cat_path}atom")
     publish_more(cat.category_type, :uri => uri, :path => path, :dependent => "#{cat_path}more")
+    publish_more(cat.category_type, :uri => uri, :path => smart_phone_path, :dependent => "#{cat_path}more_smart_phone", :smart_phone => true)
     publish_more(cat.category_type, :uri => uri, :path => path, :file => 'more', :dependent => "#{cat_path}more_docs")
+    publish_more(cat.category_type, :uri => uri, :path => smart_phone_path, :file => 'more', :dependent => "#{cat_path}more_docs_smart_phone", :smart_phone => true)
 
     if feed_pieces = category_feed_pieces(cat)
       feed_pieces.each do |piece|
