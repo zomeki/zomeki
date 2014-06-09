561984e6346fd5a790e9ac3d4032515366fce678
zomeki <zomeki@zomeki.jp>
2014-06-09 15:23:43 +0900

    Fix publication without public node

--- app/controllers/gp_category/admin/tool/category_types_controller.rb
+++ app/controllers/gp_category/admin/tool/category_types_controller.rb
@@ -5,16 +5,21 @@ class GpCategory::Admin::Tool::CategoryTypesController < Cms::Controller::Admin:
     results = {ok: 0, ng: 0}
     errors = []
 
-    content.category_types.each do |category_type|
-      begin
-        render_component_into_view :controller => '/gp_category/script/category_types', :action => 'publish',
-                                   :params => {target_id: [category_type.id], node_id: content.public_node.id}
-        results[:ok] += 1
-      rescue => e
-        results[:ng] += 1
-        errors << "エラー： #{category_type.id}, #{category_type.title}, #{e.message}"
-        error_log("Rebuild: #{e.message}")
+    if content.public_node
+      content.category_types.each do |category_type|
+        begin
+          render_component_into_view :controller => '/gp_category/script/category_types', :action => 'publish',
+                                     :params => {target_id: [category_type.id], node_id: content.public_node.id}
+          results[:ok] += 1
+        rescue => e
+          results[:ng] += 1
+          errors << "エラー： #{category_type.id}, #{category_type.title}, #{e.message}"
+          error_log("Rebuild: #{e.message}")
+        end
       end
+    else
+      results[:ng] += 1
+      errors << 'エラー： ディレクトリが作成されていません。'
     end
 
     messages = ["-- 成功 #{results[:ok]}件", "-- 失敗 #{results[:ng]}件"]
