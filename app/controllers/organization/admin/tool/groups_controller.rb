c9d442fbf827945956d4af6af61263d1847a44db
zomeki <zomeki@zomeki.jp>
2014-06-09 15:24:39 +0900

    Use common method to request as smartphone

--- app/controllers/organization/admin/tool/groups_controller.rb
+++ app/controllers/organization/admin/tool/groups_controller.rb
@@ -1,10 +1,4 @@
 class Organization::Admin::Tool::GroupsController < Cms::Controller::Admin::Base
-  def pre_dispatch
-    user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_1 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D201 Safari/9537.53'
-    jpmobile = Jpmobile::Mobile::AbstractMobile.carrier('HTTP_USER_AGENT' => user_agent)
-    @jpmobile = {'HTTP_USER_AGENT' => user_agent, 'rack.jpmobile' => jpmobile}
-  end
-
   def rebuild
     content = Organization::Content::Group.find(params[:content_id])
 
@@ -14,7 +8,7 @@ class Organization::Admin::Tool::GroupsController < Cms::Controller::Admin::Base
     content.groups.each do |group|
       begin
         if group.rebuild(render_public_as_string("#{group.public_uri}index.html", site: content.site))
-          rendered = render_public_as_string("#{group.public_uri}index.html", site: content.site, jpmobile: @jpmobile)
+          rendered = render_public_as_string("#{group.public_uri}index.html", site: content.site, jpmobile: envs_to_request_as_smart_phone)
           group.publish_page(rendered, path: group.public_smart_phone_path)
 
           results[:ok] += 1
