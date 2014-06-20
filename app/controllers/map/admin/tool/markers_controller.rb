e70a2d156f1dd4abb27fc6642fc1fdffc6b9ec4c
zomeki <zomeki@zomeki.jp>
2014-06-20 09:37:45 +0900

    Update publish and rebuild map with pagination

--- app/controllers/map/admin/tool/markers_controller.rb
+++ app/controllers/map/admin/tool/markers_controller.rb
@@ -7,27 +7,8 @@ class Map::Admin::Tool::MarkersController < Cms::Controller::Admin::Base
 
     content.public_nodes.each do |node|
       begin
-        node.publish_page(render_public_as_string(node.public_uri, site: node.site))
-
-        public_file_path = "#{node.public_path}index.html"
-        public_smart_phone_file_path = "#{node.public_smart_phone_path}index.html"
-
-        if ::File.exist?(public_file_path)
-          rendered = render_public_as_string(node.public_uri, site: node.site, jpmobile: envs_to_request_as_smart_phone)
-          Util::File.put(public_smart_phone_file_path, :data => rendered, :mkdir => true)
-        else
-          FileUtils.rm public_smart_phone_file_path if ::File.exist?(public_smart_phone_file_path)
-          FileUtils.rmdir node.public_smart_phone_path
-        end
-
-        content.public_markers.each do |marker|
-          file = marker.files.first
-          next unless file && ::File.exist?(file.upload_path)
-
-          Util::File.put marker.public_file_path, src: file.upload_path, mkdir: true
-          Util::File.put marker.public_smart_phone_file_path, src: file.upload_path, mkdir: true
-        end
-
+        render_component_into_view :controller => '/map/script/markers', :action => 'publish',
+                                   :params => {node_id: node.id}
         results[:ok] += 1
       rescue => e
         results[:ng] += 1
