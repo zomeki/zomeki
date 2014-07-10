9f395c1ef5a90b1d143e8e0b4fa18cb2d3daa7e4
m-miki <miki@idsinc.co.jp>
2014-07-10 22:51:05 +0900

    add rsync error handling (file-transfer)

--- lib/plugins/zomeki_plugin/lib/extend_rsync.rb
+++ lib/plugins/zomeki_plugin/lib/extend_rsync.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 module Rsync
   class Change
-
     def file_type
       t = case raw_file_type
         when 'f'
@@ -21,6 +20,15 @@ module Rsync
       return :file if update_type == :message && message == 'deleting'
       t
     end
+  end
 
+  class Result
+    def status_code
+      @exitcode
+    end
+
+    def status
+      {:success => success?, :code => @exitcode, :message => error }
+    end
   end
 end
