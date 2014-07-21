b9b004fe0f9b158c2c924bb3a4a72962b472a35a
zomeki <zomeki@zomeki.jp>
2014-07-21 23:05:09 +0900

    Fix placeholder styles

--- app/models/tag/content/tag.rb
+++ app/models/tag/content/tag.rb
@@ -22,7 +22,7 @@ class Tag::Content::Tag < Cms::Content
   private
 
   def set_default_settings
-    in_settings[:list_style] = '@title(@date @group)' unless setting_value(:list_style)
+    in_settings[:list_style] = '@title@(@date@ @group@)' unless setting_value(:list_style)
     in_settings[:date_style] = '%Y年%m月%d日 %H時%M分' unless setting_value(:date_style)
   end
 end
