07c61839ca9c543ff874d12220b4df100d749b79
zomeki <zomeki@zomeki.jp>
2014-07-29 00:30:25 +0900

    Publish node after updating

--- app/controllers/gp_article/admin/node/docs_controller.rb
+++ app/controllers/gp_article/admin/node/docs_controller.rb
@@ -1,2 +1,10 @@
 class GpArticle::Admin::Node::DocsController < Cms::Admin::Node::BaseController
+  after_filter :publish_node, only: [:update]
+
+  private
+
+  def publish_node
+    ::Script.delay(queue: "publish_node_#{@item.id}")
+            .run("cms/script/nodes/publish?target_module=cms&target_node_id=#{@item.id}", force: true)
+  end
 end
