dd5d1a6fd38dda903bd9702756e50ce7caba9a70
zomeki <zomeki@zomeki.jp>
2014-07-29 23:17:25 +0900

    Remove nodes republication from directories

--- app/controllers/gp_article/admin/node/docs_controller.rb
+++ app/controllers/gp_article/admin/node/docs_controller.rb
@@ -1,10 +1,2 @@
 class GpArticle::Admin::Node::DocsController < Cms::Admin::Node::DirectoriesController
-  after_filter :publish_node, only: [:update]
-
-  private
-
-  def publish_node
-    ::Script.delay(queue: "publish_node_#{@item.id}")
-            .run("cms/script/nodes/publish?target_module=cms&target_node_id=#{@item.id}", force: true)
-  end
 end
