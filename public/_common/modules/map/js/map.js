c4194186b0e7d21fa31c13c9c64e79272da86176
zomeki <zomeki@zomeki.jp>
2014-08-26 15:54:22 +0900

    Change map zoom level on click

--- public/_common/modules/map/js/map.js
+++ public/_common/modules/map/js/map.js
@@ -70,6 +70,7 @@ Map.prototype.move_to = function (id) {
     var destination = this._markers[id];
     if (destination) {
       this._map_canvas.panTo(destination.position);
+      this._map_canvas.setZoom(12);
       destination.open_info_window();
     }
   }
