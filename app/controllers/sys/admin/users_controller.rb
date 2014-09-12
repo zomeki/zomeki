233da1a848ad49a9d1e59648b3b2174b9037b042
mmukai <mukai@idsinc.co.jp>
2014-09-12 18:34:36 +0900

    Undisplayed in system administrator at user lists

--- app/controllers/sys/admin/users_controller.rb
+++ app/controllers/sys/admin/users_controller.rb
@@ -13,6 +13,7 @@ class Sys::Admin::UsersController < Cms::Controller::Admin::Base
     
     item = Sys::User.new
     item.search params
+    item.and 'sys_users.id', '<>', 1 unless Core.user.root?
     item.page  params[:page], params[:limit]
     item.order params[:sort], "LPAD(account, 15, '0')"
 
@@ -27,6 +28,7 @@ class Sys::Admin::UsersController < Cms::Controller::Admin::Base
   def show
     @item = Sys::User.new.find(params[:id])
     return error_auth unless @item.readable?
+    return error_auth if !Core.user.root? && @item.root?
     
     _show @item
   end
@@ -52,12 +54,14 @@ class Sys::Admin::UsersController < Cms::Controller::Admin::Base
   
   def update
     @item = Sys::User.new.find(params[:id])
+    return error_auth if !Core.user.root? && @item.root?
     @item.attributes = params[:item]
     _update(@item)
   end
   
   def destroy
     @item = Sys::User.new.find(params[:id])
+    return error_auth if !Core.user.root? && @item.root?
     _destroy(@item)
   end
 end
