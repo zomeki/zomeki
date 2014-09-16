9971fa8d23e3df29fd83316a15404410210c15fa
zomeki <zomeki@zomeki.jp>
2014-09-17 00:13:11 +0900

    Now use common replace texts on setting

--- app/models/gp_category/content/setting.rb
+++ app/models/gp_category/content/setting.rb
@@ -3,7 +3,7 @@ class GpCategory::Content::Setting < Cms::ContentSetting
   set_config :group_category_type_name, :name => "組織用#{GpCategory::CategoryType.human_attribute_name :name}",
     :comment => '初期値 ： groups'
   set_config :list_style, :name => "#{GpArticle::Doc.model_name.human}表示形式",
-    :form_type => :text_area, :comment_upper => I18n.t('comments.doc_style').html_safe
+    :form_type => :text_area, :comment_upper => 'doc_style_tags'
   set_config :date_style, :name => "#{GpArticle::Doc.model_name.human}日付形式",
     :comment => I18n.t('comments.date_style').html_safe
   set_config :time_style, :name => "#{GpArticle::Doc.model_name.human}時間形式",
