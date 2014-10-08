d751addc40c2456d9572280b1923538c68762cd1
zomeki <zomeki@zomeki.jp>
2014-10-08 12:35:36 +0900

    Add static publication for Tag

--- config/modules/tag/routes.rb
+++ config/modules/tag/routes.rb
@@ -1,6 +1,9 @@
 ZomekiCMS::Application.routes.draw do
   mod = 'tag'
 
+  ## script
+  get "/_script/#{mod}/script/tags/publish" => "#{mod}/script/tags#publish"
+
   ## admin
   scope "#{ZomekiCMS::ADMIN_URL_PREFIX}/#{mod}", :module => mod, :as => mod do
     resources :content_base,
