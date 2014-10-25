6e4f69ca65ec26834e7b0f8ea946fbdaee223057
zomeki <zomeki@zomeki.jp>
2014-10-25 23:47:01 +0900

    Add static publication for Gnav

--- app/controllers/gnav/script/menu_items_controller.rb
+++ app/controllers/gnav/script/menu_items_controller.rb
@@ -1,6 +1,17 @@
 class Gnav::Script::MenuItemsController < Cms::Controller::Script::Publication
   def publish
-    info_log 'Gnav::Script::MenuItemsController#publish'
+    uri = @node.public_uri.to_s
+    path = @node.public_path.to_s
+    smart_phone_path = @node.public_smart_phone_path.to_s
+    publish_more(@node, uri: uri, path: path, smart_phone_path: smart_phone_path, dependent: uri)
+
+    @node.content.menu_items.each do |menu_item|
+      mi_uri = menu_item.public_uri
+      mi_path = menu_item.public_path
+      mi_smart_phone_path = menu_item.public_smart_phone_path
+      publish_more(@node, uri: mi_uri, path: mi_path, smart_phone_path: mi_smart_phone_path, dependent: mi_uri)
+    end
+
     render text: 'OK'
   rescue => e
     error_log e.message
