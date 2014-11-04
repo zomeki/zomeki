f36efeef607231c6c4fdf1dba279c3f11d2aed27
zomeki <zomeki@zomeki.jp>
2014-11-05 01:17:49 +0900

    GpCalendar now accepts category via filename

--- config/modules/gp_calendar/routes.rb
+++ config/modules/gp_calendar/routes.rb
@@ -52,11 +52,17 @@ ZomekiCMS::Application.routes.draw do
 
   ## public
   scope "_public/#{mod}", :module => mod, :as => '' do
+    match 'node_calendar_styled_events/:year/:month/index_:escaped_category' => 'public/node/calendar_styled_events#index'
     match 'node_calendar_styled_events/:year/:month(/index)' => 'public/node/calendar_styled_events#index'
+    match 'node_calendar_styled_events/:year/index_:escaped_category' => 'public/node/calendar_styled_events#index'
     match 'node_calendar_styled_events/:year(/index)' => 'public/node/calendar_styled_events#index'
+    match 'node_calendar_styled_events/index_:escaped_category' => 'public/node/calendar_styled_events#index'
     match 'node_calendar_styled_events(/index)' => 'public/node/calendar_styled_events#index'
+    match 'node_events/:year/:month/index_:escaped_category' => 'public/node/events#index'
     match 'node_events/:year/:month(/index)' => 'public/node/events#index'
+    match 'node_events/:year/index_:escaped_category' => 'public/node/events#index'
     match 'node_events/:year(/index)' => 'public/node/events#index'
+    match 'node_events/index_:escaped_category' => 'public/node/events#index'
     match 'node_events(/index)' => 'public/node/events#index'
     match 'node_events/:name/file_contents/:basename.:extname' => 'public/node/events#file_content', :format => false
     match 'node_todays_events(/index)' => 'public/node/todays_events#index'
