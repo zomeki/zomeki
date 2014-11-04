74635f6a6c1ea014553ce944761178e0c26c2371
zomeki <zomeki@zomeki.jp>
2014-11-05 01:35:58 +0900

    Fix routes for gp_calendar

--- config/modules/gp_calendar/routes.rb
+++ config/modules/gp_calendar/routes.rb
@@ -52,19 +52,19 @@ ZomekiCMS::Application.routes.draw do
 
   ## public
   scope "_public/#{mod}", :module => mod, :as => '' do
-    match 'node_calendar_styled_events/:year/:month/index_:escaped_category' => 'public/node/calendar_styled_events#index'
-    match 'node_calendar_styled_events/:year/:month(/index)' => 'public/node/calendar_styled_events#index'
-    match 'node_calendar_styled_events/:year/index_:escaped_category' => 'public/node/calendar_styled_events#index'
-    match 'node_calendar_styled_events/:year(/index)' => 'public/node/calendar_styled_events#index'
-    match 'node_calendar_styled_events/index_:escaped_category' => 'public/node/calendar_styled_events#index'
-    match 'node_calendar_styled_events(/index)' => 'public/node/calendar_styled_events#index'
-    match 'node_events/:year/:month/index_:escaped_category' => 'public/node/events#index'
-    match 'node_events/:year/:month(/index)' => 'public/node/events#index'
-    match 'node_events/:year/index_:escaped_category' => 'public/node/events#index'
-    match 'node_events/:year(/index)' => 'public/node/events#index'
-    match 'node_events/index_:escaped_category' => 'public/node/events#index'
-    match 'node_events(/index)' => 'public/node/events#index'
-    match 'node_events/:name/file_contents/:basename.:extname' => 'public/node/events#file_content', :format => false
-    match 'node_todays_events(/index)' => 'public/node/todays_events#index'
+    get 'node_calendar_styled_events/index_:escaped_category' => 'public/node/calendar_styled_events#index'
+    get 'node_calendar_styled_events/:year/index_:escaped_category' => 'public/node/calendar_styled_events#index'
+    get 'node_calendar_styled_events/:year/:month/index_:escaped_category' => 'public/node/calendar_styled_events#index'
+    get 'node_calendar_styled_events/:year/:month(/index)' => 'public/node/calendar_styled_events#index'
+    get 'node_calendar_styled_events/:year(/index)' => 'public/node/calendar_styled_events#index'
+    get 'node_calendar_styled_events(/index)' => 'public/node/calendar_styled_events#index'
+    get 'node_events/index_:escaped_category' => 'public/node/events#index'
+    get 'node_events/:year/index_:escaped_category' => 'public/node/events#index'
+    get 'node_events/:year/:month/index_:escaped_category' => 'public/node/events#index'
+    get 'node_events/:year/:month(/index)' => 'public/node/events#index'
+    get 'node_events/:year(/index)' => 'public/node/events#index'
+    get 'node_events(/index)' => 'public/node/events#index'
+    get 'node_events/:name/file_contents/:basename.:extname' => 'public/node/events#file_content', :format => false
+    get 'node_todays_events(/index)' => 'public/node/todays_events#index'
   end
 end
