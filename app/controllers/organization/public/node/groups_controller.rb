b83a0105d5f494a1676ebfab4d8e47adf40871d8
zomeki <zomeki@zomeki.jp>
2014-11-04 14:48:12 +0900

    Add static publication to organization docs

--- app/controllers/organization/public/node/groups_controller.rb
+++ app/controllers/organization/public/node/groups_controller.rb
@@ -13,7 +13,7 @@ class Organization::Public::Node::GroupsController < Cms::Controller::Public::Ba
   end
 
   def show
-    http_error(404) if params[:page]
+    http_error(404) if params[:page] && !@more
 
     @group = @content.find_group_by_path_from_root(params[:group_names])
     return http_error(404) unless @group.try(:public?)
