ce658775ab79a2e8a2c58d8b5f29cd9d3874966a
zomeki <zomeki@zomeki.jp>
2014-11-04 15:26:11 +0900

    Add blank index for static publication to Tag

--- app/controllers/tag/public/node/tags_controller.rb
+++ app/controllers/tag/public/node/tags_controller.rb
@@ -7,6 +7,8 @@ class Tag::Public::Node::TagsController < Cms::Controller::Public::Base
   end
 
   def index
+    return render(text: '') if Core.publish
+
     if @content.tags.empty?
       http_error(404)
     else
