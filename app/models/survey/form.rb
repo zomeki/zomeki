d013cfb38ba4cc88b5bd37e41ab751a6994f3052
m-miki <miki@idsinc.co.jp>
2014-11-14 14:33:33 +0900

    Add protocol setting of admin uri.

--- app/models/survey/form.rb
+++ app/models/survey/form.rb
@@ -124,8 +124,7 @@ class Survey::Form < ActiveRecord::Base
 
   def send_approval_request_mail
 
-    d = Zomeki.config.application['sys.core_domain']
-    _core_uri = (d == 'core') ? Core.full_uri : content.site.full_uri;
+    _core_uri = Cms::SiteSetting::AdminProtocol.core_domain content.site, content.site.full_uri
 
     approve_url = "#{_core_uri.sub(/\/+$/, '')}#{Rails.application.routes.url_helpers.survey_form_path(content: content, id: id)}"
     preview_url = self.preview_uri
@@ -141,8 +140,7 @@ class Survey::Form < ActiveRecord::Base
 
   def send_approved_notification_mail
 
-    d = Zomeki.config.application['sys.core_domain']
-    _core_uri = (d == 'core') ? Core.full_uri : content.site.full_uri;
+    _core_uri = Cms::SiteSetting::AdminProtocol.core_domain content.site, content.site.full_uri
 
     publish_url = "#{_core_uri.sub(/\/+$/, '')}#{Rails.application.routes.url_helpers.survey_form_path(content: content, id: id)}"
 
@@ -232,8 +230,9 @@ class Survey::Form < ActiveRecord::Base
     site ||= ::Page.site
     params = params.map{|k, v| "#{k}=#{v}" }.join('&')
     path = "_preview/#{format('%08d', site.id)}#{mobile ? 'm' : ''}#{public_uri}#{params.present? ? "?#{params}" : ''}"
-    d = Zomeki.config.application['sys.core_domain']
-    d == 'core' ? "#{Core.full_uri}#{path}" : "#{site.full_uri}#{path}";
+
+    d = Cms::SiteSetting::AdminProtocol.core_domain site, site.full_uri, :freeze_protocol => true
+    "#{d}#{path}"
   end
 
   def sitemap_visible?
