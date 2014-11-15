7a41c0fab5fdcb3175a55dfacbfa934fe8480ad2
zomeki <zomeki@zomeki.jp>
2014-11-15 22:17:38 +0900

    Fix ranking pagination

--- app/controllers/rank/public/node/previous_days_controller.rb
+++ app/controllers/rank/public/node/previous_days_controller.rb
@@ -10,11 +10,12 @@ class Rank::Public::Node::PreviousDaysController < Cms::Controller::Public::Base
   end
 
   def index
-    http_error(404) if params[:page]
-
     @term   = 'previous_days'
     @target = 'pageviews'
     @ranks  = rank_datas(@content, @term, @target, 20)
+
+    return http_error(404) if @ranks.blank?
+
     _index @ranks
   end
 end
