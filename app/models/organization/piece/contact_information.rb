46a8a8db18d4f4ceef53624d875df1532148989c
zomeki <zomeki@zomeki.jp>
2015-01-05 19:48:14 +0900

    Set default source to contact_information piece

--- app/models/organization/piece/contact_information.rb
+++ app/models/organization/piece/contact_information.rb
@@ -3,11 +3,23 @@ class Organization::Piece::ContactInformation < Cms::Piece
 
   default_scope { where(model: 'Organization::ContactInformation') }
 
+  after_initialize :set_defaults
+
   def source
     setting_value(:source) || SOURCE_OPTIONS.first.last
   end
 
   def source_text
     SOURCE_OPTIONS.detect{|o| o.last == source }.try(:first).to_s
+  end
+
+  private
+
+  def set_defaults
+    is = self.in_settings
+
+    is[:source] ||= source
+
+    self.in_settings = is
   end
 end
