37a5798c049f6235c310729e6b98ab2f7dfd0e71
zomeki <zomeki@zomeki.jp>
2015-01-05 11:50:01 +0900

    Add source option to contact_information piece

--- app/models/organization/piece/contact_information.rb
+++ app/models/organization/piece/contact_information.rb
@@ -1,3 +1,13 @@
 class Organization::Piece::ContactInformation < Cms::Piece
+  SOURCE_OPTIONS = [['組織コンテンツ', 'organization_group'], ['グループ', 'sys_group']]
+
   default_scope { where(model: 'Organization::ContactInformation') }
+
+  def source
+    setting_value(:source) || SOURCE_OPTIONS.first.last
+  end
+
+  def source_text
+    SOURCE_OPTIONS.detect{|o| o.last == source }.try(:first).to_s
+  end
 end
