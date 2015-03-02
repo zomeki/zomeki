37aa44fecc726ce3c23956e7dc2abe396b0bb33a
mmukai <mukai@idsinc.co.jp>
2015-03-02 13:12:35 +0900

    Fixed calendar piece of GpCalendar

--- app/controllers/biz_calendar/public/piece/calendars_controller.rb
+++ app/controllers/biz_calendar/public/piece/calendars_controller.rb
@@ -8,7 +8,7 @@ class BizCalendar::Public::Piece::CalendarsController < BizCalendar::Public::Pie
   end
 
   def index
-    return render(:text => 'eeeee') unless @place = @piece.place
+    return render(:text => '') unless @place = @piece.place
     @today = Date.today
 
     @months = []
