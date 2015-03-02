e38fdc746fcb7ae36f8e9ef5c1c60bd9a9cbae95
mmukai <mukai@idsinc.co.jp>
2015-03-02 16:49:57 +0900

    Fixed time format settings of Biz_Calendar

--- app/models/biz_calendar/content/setting.rb
+++ app/models/biz_calendar/content/setting.rb
@@ -5,6 +5,6 @@ class BizCalendar::Content::Setting < Cms::ContentSetting
   set_config :date_style, name: "日付形式",
     comment: I18n.t('comments.date_style').html_safe
   set_config :time_style, name: "時間形式",
-    comment: I18n.t('comments.time_style').html_safe
+    comment: "<strong>午前/午後</strong>：%P <strong>時：</strong>%H <strong>分：</strong>%M <strong>秒：</strong>%S".html_safe
   
 end
