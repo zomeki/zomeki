469c6bbdd5f004a014f677f6229d9b874d05af65
zomeki <zomeki@zomeki.jp>
2015-03-04 16:25:13 +0900

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150302025720) do
+ActiveRecord::Schema.define(:version => 20150304071519) do
 
   create_table "ad_banner_banners", :force => true do |t|
     t.string   "name"
@@ -889,7 +889,7 @@ ActiveRecord::Schema.define(:version => 20150302025720) do
     t.datetime "updated_at",               :null => false
     t.string   "sync_source_host"
     t.integer  "sync_source_content_id"
-    t.integer  "sync_source_id"
+    t.string   "sync_source_id"
     t.string   "sync_source_source_class"
   end
 
