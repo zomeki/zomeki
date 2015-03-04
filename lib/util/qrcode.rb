5773bb8a74753a4dfb93a5e19682e4aa8718c184
zomeki <zomeki@zomeki.jp>
2015-03-04 09:35:30 +0900

    Increase default size to fix "code length overflow."

--- lib/util/qrcode.rb
+++ lib/util/qrcode.rb
@@ -3,28 +3,30 @@ class Util::Qrcode
 
   require 'rqrcode_png'
 
-  def self.create(text=nil, path=nil, size=6)
+  def self.create(text=nil, path=nil, size=8)
     return nil if text.blank? || path.blank?
 
     begin
       qr = RQRCode::QRCode.new(text, :size => size, :level => :h )
       png = qr.to_img
       png.resize(114, 114).save(path)
-    rescue
+    rescue => e
+      warn_log "#{__FILE__}:#{__LINE__} #{e.message}"
       return nil
     end
     
   end
 
 
-  def self.create_date(text=nil, path=nil, size=6)
+  def self.create_date(text=nil, path=nil, size=8)
     return nil if text.blank? || path.blank?
 
     begin
       qr = RQRCode::QRCode.new(text, :size => size, :level => :h )
       png = qr.to_img
       return png.resize(114, 114).to_blob
-    rescue
+    rescue => e
+      warn_log "#{__FILE__}:#{__LINE__} #{e.message}"
       return nil
     end
     return nil
