96a9cb0817e3a544f576c86b1488caca7000d13b
mmukai <mukai@sitebridge.co.jp>
2015-07-31 14:15:30 +0900

    Add the display type setting to holiday list piece of Biz::Calendar

--- app/models/biz_calendar/piece/bussiness_holiday.rb
+++ app/models/biz_calendar/piece/bussiness_holiday.rb
@@ -3,6 +3,7 @@ class BizCalendar::Piece::BussinessHoliday < Cms::Piece
   TARGET_TYPE_OPTIONS = [['全て', 'all'], ['次回', 'next']]
   PAGE_FILTER_OPTIONS = [['絞り込む', 'filter'], ['絞り込まない', 'through']]
   PLACE_LINK_OPTIONS = [['あり', 'enabled'], ['なし', 'disabled']]
+  HOLIDAY_TYPE_STATE_OPTIONS = [['表示する','visible'],['表示しない','hidden']]
 
   default_scope where(model: 'BizCalendar::BussinessHoliday')
 
@@ -36,6 +37,14 @@ class BizCalendar::Piece::BussinessHoliday < Cms::Piece
     setting_value(:date_style).presence || '%Y年%m月%d日'
   end
 
+  def holiday_type_state
+    setting_value(:holiday_type_state).presence || 'hidden'
+  end
+
+  def show_holiday_type?
+    holiday_type_state == 'visible'
+  end
+
   private
 
   def set_default_settings
@@ -44,6 +53,7 @@ class BizCalendar::Piece::BussinessHoliday < Cms::Piece
     settings['page_filter'] = 'filter' if setting_value(:page_filter).nil?
     settings['date_style'] = '%Y年%m月%d日' if setting_value(:date_style).nil?
     settings['place_link'] = 'enabled' if setting_value(:place_link).nil?
+    settings['holiday_type_state'] = 'hidden' if setting_value(:holiday_type_state).nil?
 
     self.in_settings = settings
   end
