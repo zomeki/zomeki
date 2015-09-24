10060b004ea96a65f880df97a5f6335ba37e85e7
mmukai <mukai@sitebridge.co.jp>
2015-09-24 14:54:37 +0900

    Add a banner image writing processing

--- app/controllers/ad_banner/script/banners_controller.rb
+++ app/controllers/ad_banner/script/banners_controller.rb
@@ -2,4 +2,40 @@ class AdBanner::Script::BannersController < Cms::Controller::Script::Publication
   def publish
     render text: 'OK'
   end
+  
+  def publish_by_task
+    item = params[:item]
+    if item && item.state == 'public'
+      Script.current
+      info_log "-- Publish: #{item.class}##{item.id}"
+
+      item.publish_image
+
+      info_log %Q!OK: Published to "#{item.image_path}"!
+      params[:task].destroy
+      Script.success
+    end
+    render text: 'OK'
+  rescue => e
+    error_log "#{__FILE__}:#{__LINE__} #{e.message}"
+    render text: 'NG'
+  end
+  
+  def close_by_task
+    item = params[:item]
+    if item && item.state == 'public'
+      Script.current
+      info_log "-- Close: #{item.class}##{item.id}"
+
+      item.close_image
+
+      info_log 'OK: Closed'
+      params[:task].destroy
+      Script.success
+    end
+    render text: 'OK'
+  rescue => e
+    error_log "#{__FILE__}:#{__LINE__} #{e.message}"
+    render text: 'NG'
+  end
 end
